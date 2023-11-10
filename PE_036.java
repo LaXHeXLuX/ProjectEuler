dd96fe72c03e544ccd6d72c7e1bb65acea86049c
LaXHeXLuX <laas.lumberg@gmail.com>
2023-11-10 20:55:17 +0200

    Added 36

---
+++ PE_036.java
@@ -0,0 +1,47 @@
+import UsefulFunctions.Converter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PE_036 {
+    public static void main(String[] args) {
+        int[] bases = {2, 10};
+        int limit = 1_000_000;
+        long[] palindromes = findPalindromesInBases(limit, bases);
+
+        long sum = 0;
+        for (long palindrome : palindromes) sum += palindrome;
+        System.out.println(Arrays.toString(palindromes));
+        System.out.println(sum);
+    }
+
+    private static boolean isPalindrome(int[] digitArray) {
+        for (int i = 0; i < digitArray.length/2; i++) {
+            if (digitArray[i] != digitArray[digitArray.length-i-1]) return false;
+        }
+        return true;
+    }
+
+    private static boolean isPalindromeInBases(long n, int[] bases) {
+        Converter c = new Converter();
+
+        for (int base : bases) {
+            int[] baseArray = c.convertToBase(n, base);
+            if (!isPalindrome(baseArray)) return false;
+        }
+
+        return true;
+    }
+
+    private static long[] findPalindromesInBases(long limit, int[] bases) {
+        List<Long> palindromes = new ArrayList<>();
+
+        for (long i = 0; i < limit; i++) {
+            if (isPalindromeInBases(i, bases)) palindromes.add(i);
+        }
+
+        Converter c = new Converter();
+        return c.listToArrLong(palindromes);
+    }
+}
