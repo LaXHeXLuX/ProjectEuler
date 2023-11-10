5d0c36dbba72a3d046c0c1953613fc91710f7613
LaXHeXLuX <laas.lumberg@gmail.com>
2023-11-10 17:34:09 +0200

    Added Pandigital class to UsefulFunctions

---
+++ UsefulFunctions/Pandigital.java
@@ -0,0 +1,75 @@
+package UsefulFunctions;
+
+public class Pandigital {
+    public boolean isPandigital(long n) {
+        Converter c = new Converter();
+        int[] digits = c.digitArray(n);
+
+        ArrayFunctions af = new ArrayFunctions();
+        digits = af.mergeSort(digits);
+
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] != i+1) return false;
+        }
+        return true;
+    }
+    public boolean isPandigital(int[] digits) {
+        ArrayFunctions af = new ArrayFunctions();
+        digits = af.mergeSort(digits);
+
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] != i+1) return false;
+        }
+        return true;
+    }
+    public boolean isPandigital(long[] digits) {
+        ArrayFunctions af = new ArrayFunctions();
+        digits = af.mergeSort(digits);
+
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] != i+1) return false;
+        }
+        return true;
+    }
+    public boolean groupIsPandigital(int[] numbers) {
+        int[][] groupDigits = new int[numbers.length][];
+        Converter c = new Converter();
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            groupDigits[i] = c.digitArray(numbers[i]);
+            sum += groupDigits[i].length;
+        }
+
+        return groupIsPandigitalHelper(groupDigits, sum);
+    }
+    public boolean groupIsPandigital(long[] numbers) {
+        int[][] groupDigits = new int[numbers.length][];
+        Converter c = new Converter();
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            groupDigits[i] = c.digitArray(numbers[i]);
+            sum += groupDigits[i].length;
+        }
+
+        return groupIsPandigitalHelper(groupDigits, sum);
+    }
+    private boolean groupIsPandigitalHelper(int[][] groupDigits, int sum) {
+        if (sum != 9) return false;
+        int[] digits = new int[sum];
+        int index = 0;
+        for (int[] groupDigit : groupDigits) {
+            for (int i : groupDigit) {
+                digits[index] = i;
+                index++;
+            }
+        }
+
+        ArrayFunctions af = new ArrayFunctions();
+        digits = af.mergeSort(digits);
+
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] != i+1) return false;
+        }
+        return true;
+    }
+}
