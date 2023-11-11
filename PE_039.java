94c53eb05665127267c91951e75a29f0b9297302
LaXHeXLuX <laas.lumberg@gmail.com>
2023-11-11 07:01:31 +0200

    Added 39, 40

---
+++ PE_039.java
@@ -0,0 +1,48 @@
+import UsefulFunctions.Converter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PE_039 {
+    public static void main(String[] args) {
+        int limit = 1_001;
+        int mostTriangles = sumWitMostTriangles(limit);
+        System.out.println(mostTriangles);
+        int[][] triangles = findAllTrianglesForSum(mostTriangles);
+        for (int[] triangle : triangles) System.out.println(Arrays.toString(triangle));
+    }
+
+    private static boolean isTriangle(int a, int b, int c) {
+        return a*a + b*b == c*c;
+    }
+
+    private static int[][] findAllTrianglesForSum(int sum) {
+        List<int[]> triangles = new ArrayList<>();
+
+        for (int a = 1; a < sum/3; a++) {
+            for (int b = a+1; b < sum/2; b++) {
+                int c = sum - a - b;
+                if (isTriangle(a, b, c)) triangles.add(new int[] {a, b, c});
+            }
+        }
+
+        Converter c = new Converter();
+        return c.arrListToArrInt(triangles);
+    }
+
+    private static int sumWitMostTriangles(int limit) {
+        int mostTriangles = 0;
+        int bestSum = 0;
+
+        for (int i = 0; i < limit; i++) {
+            int[][] triangles = findAllTrianglesForSum(i);
+            if (triangles.length > mostTriangles) {
+                mostTriangles = triangles.length;
+                bestSum = i;
+            }
+        }
+
+        return bestSum;
+    }
+}
---
+++ UsefulFunctions/PE_040.java
@@ -0,0 +1,51 @@
+package UsefulFunctions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PE_040 {
+    public static void main(String[] args) {
+        int[] indexes = {1, 10, 100, 1_000, 10_000, 100_000, 1_000_000};
+        for (int i = 0; i < indexes.length; i++) {
+            indexes[i]--;
+        }
+        System.out.println(productOfConstantDigits(indexes));
+    }
+
+    private static int[] constantDigitsUntil(int limit) {
+        List<Integer> constantDigits = new ArrayList<>();
+        Converter c = new Converter();
+
+        int i = 1;
+        while (constantDigits.size() < limit) {
+            int[] digits = c.digitArray(i);
+            for (int digit : digits) constantDigits.add(digit);
+            i++;
+        }
+
+        return c.listToArrInt(constantDigits);
+    }
+
+    private static long productOfConstantDigits(int[] indexes) {
+        long product = 1;
+
+        int[] constantDigits = constantDigitsUntil(max(indexes)+1);
+
+        for (int index : indexes) {
+            product *= constantDigits[index];
+            System.out.println(index + ", " + constantDigits[index] + ", " + product);
+        }
+
+        return product;
+    }
+
+    private static int max(int[] arr) {
+        int max = arr[0];
+
+        for (int a : arr) {
+            if (a > max) max = a;
+        }
+
+        return max;
+    }
+}
