096d927009c0ee29d4ba4ff3d6573df5d4b220c5
LaXHeXLuX <laas.lumberg@gmail.com>
2023-11-12 17:34:22 +0200

    Improved Combinations, ArrayFunctions

--- UsefulFunctions/ArrayFunctions.java
+++ UsefulFunctions/ArrayFunctions.java
@@ -188,4 +188,10 @@ public class ArrayFunctions {
         System.arraycopy(arr, startIndex, newArr, 0, newArr.length);
         return newArr;
     }
+    public static int[] concatenate(int[] arr1, int[] arr2) {
+        int[] newArr = new int[arr1.length+ arr2.length];
+        System.arraycopy(arr1, 0, newArr, 0, arr1.length);
+        System.arraycopy(arr2, 0, newArr, arr1.length, arr2.length);
+        return newArr;
+    }
 }
--- UsefulFunctions/Combinations.java
+++ UsefulFunctions/Combinations.java
@@ -2,6 +2,8 @@ package UsefulFunctions;
 
 import java.math.BigInteger;
 
+import static UsefulFunctions.ArrayFunctions.concatenate;
+
 public class Combinations {
 
     public static BigInteger factorialBigInteger(int n) {
@@ -27,5 +29,44 @@ public class Combinations {
         BigInteger factorial2 = factorialBigInteger(m);
         BigInteger factorial3 = factorialBigInteger(n-m);
         return factorial1.divide(factorial2.multiply(factorial3));
+    }
+    public static int[][] findPermutations(int[] arr) {
+        if (arr.length == 0) return new int[][] {};
+        if (arr.length == 1) return new int[][] {arr};
+        int[][] permutations = new int[(int) Combinations.factorialLong(arr.length)][];
+        arr = ArrayFunctions.mergeSort(arr);
+        int[] newArr0 = new int[arr.length];
+        System.arraycopy(arr, 0, newArr0, 0, newArr0.length);
+        permutations[0] = newArr0;
+        for (int i = 1; i < permutations.length; i++) {
+            arr = swapAndSort(arr);
+            int[] newArr = new int[arr.length];
+            System.arraycopy(arr, 0, newArr, 0, newArr.length);
+            permutations[i] = newArr;
+        }
+        return permutations;
+    }
+    private static int[] swapAndSort(int[] arr) {
+        int i;
+        for (i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] < arr[i + 1]) {
+                break;
+            }
+        }
+        int ceilingIndex = i + 1;
+        for (int j = i+1; j < arr.length; j++) {
+            if (arr[j] <= arr[i]) continue;
+            if (arr[j] < arr[ceilingIndex]) ceilingIndex = j;
+        }
+        int temp = arr[i];
+        arr[i] = arr[ceilingIndex];
+        arr[ceilingIndex] = temp;
+
+        int[] arrToBeSorted = new int[arr.length-i-1];
+        System.arraycopy(arr, i+1, arrToBeSorted, 0, arrToBeSorted.length);
+        arrToBeSorted = ArrayFunctions.mergeSort(arrToBeSorted);
+        int[] restOfArr = new int[i+1];
+        System.arraycopy(arr, 0, restOfArr, 0, restOfArr.length);
+        return concatenate(restOfArr, arrToBeSorted);
     }
 }
