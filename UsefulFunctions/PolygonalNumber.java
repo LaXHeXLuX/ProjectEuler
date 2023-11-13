7eaba7e540b1f2433f3f33f3aefd55db695fc80e
LaXHeXLuX <laas.lumberg@gmail.com>
2023-11-13 20:20:07 +0200

    Added functions

--- UsefulFunctions/PolygonalNumber.java
+++ UsefulFunctions/PolygonalNumber.java
@@ -1,7 +1,36 @@
 package UsefulFunctions;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PolygonalNumber {
-    public static long polygonalNumber(int sides, int n) {
-        return ((long) (sides - 2)*n*n - (long) (sides - 4)*n)/2;
+    public static long polygonalNumberLong(long sides, long n) {
+        return ((sides - 2)*n*n - (sides - 4)*n)/2;
+    }
+    public static BigInteger polygonalNumberBigInteger(long sides, long n) {
+        BigInteger bigN = BigInteger.valueOf(n);
+        BigInteger bigSides = BigInteger.valueOf(sides);
+        BigInteger firstPart = bigSides.subtract(BigInteger.valueOf(2)).multiply(bigN).multiply(bigN);
+        BigInteger secondPart = bigSides.subtract(BigInteger.valueOf(4)).multiply(bigN);
+
+        return firstPart.subtract(secondPart).divide(BigInteger.TWO);
+    }
+    public static boolean isPolygonalNumber(long sides, long x) {
+        if (sides == x) return true;
+        long a = sides-2;
+        long b = 4-sides;
+        long c = -2*x;
+        double n = (-b + Math.sqrt(b*b - 4*a*c))/(2*a);
+        return n == Math.floor(n);
+    }
+    public static long[] isPolygonalNumber(long x) {
+        List<Long> possibleSides = new ArrayList<>();
+
+        for (long sides = 3; sides < x; sides++) {
+            if (isPolygonalNumber(sides, x)) possibleSides.add(sides);
+        }
+
+        return Converter.listToArrLong(possibleSides);
     }
 }
