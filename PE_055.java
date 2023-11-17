f2a121e7fd146b8315bd9d0dcf36e8ce0d9b1dab
LaXHeXLuX <laas.lumberg@gmail.com>
2023-11-17 06:13:00 +0200

    Added 55, 56

---
+++ PE_055.java
@@ -0,0 +1,48 @@
+import UsefulFunctions.Converter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PE_055 {
+    public static void main(String[] args) {
+        int[] answer = findLychrelNumbersUnder10_000();
+        System.out.println(Arrays.toString(answer));
+        System.out.println(answer.length);
+    }
+
+    public static int[] findLychrelNumbersUnder10_000() {
+        List<Integer> lychrelNumbers = new ArrayList<>();
+
+        for (int i = 5; i < 10_000; i++) {
+            if (iterationsToProducePalindrome(i) < 0) lychrelNumbers.add(i);
+        }
+
+        return Converter.listToArrInt(lychrelNumbers);
+    }
+
+    public static int iterationsToProducePalindrome(long n) {
+        n += reverse(n);
+        int counter = 1;
+
+        while (counter <= 50 && !isPalindrome(n)) {
+            n += reverse(n);
+            if (n < 0) return -1;
+            counter++;
+        }
+
+        if (counter > 50) return -1;
+        return counter;
+    }
+
+    private static long reverse(long n) {
+        String number = String.valueOf(n);
+        String reversed = String.valueOf(new StringBuilder(number).reverse());
+        return Long.parseLong(reversed);
+    }
+
+    private static boolean isPalindrome(long n) { // note for the future: doing it with string isn't that much faster, maybe like 1.5x faster for bigger numbers
+        String string = String.valueOf(n);
+        return (string.contentEquals(new StringBuilder(string).reverse()));
+    }
+}
---
+++ PE_056.java
@@ -0,0 +1,50 @@
+import UsefulFunctions.Converter;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class PE_056 {
+    public static void main(String[] args) {
+        int limit = 100;
+        System.out.println(Arrays.toString(biggestDigitsSumOfPower(limit, limit)));
+    }
+
+    private static int[] biggestDigitsSumOfPower(int limitA, int limitB) {
+        int[] biggests = new int[3];
+        biggests[2] = -1;
+
+        for (int a = 0; a < limitA; a++) {
+            for (int b = 0; b < limitB; b++) {
+                BigInteger power = power(a, b);
+                int digitSum = digitSum(power);
+                if (digitSum > biggests[2]) {
+                    biggests[0] = a;
+                    biggests[1] = b;
+                    biggests[2] = digitSum;
+                }
+            }
+        }
+
+        return biggests;
+    }
+
+    private static BigInteger power(int a, int b) {
+        BigInteger answer = BigInteger.valueOf(a);
+        b--;
+
+        for (int i = 0; i < b; i++) {
+            answer = answer.multiply(BigInteger.valueOf(a));
+        }
+
+        return answer;
+    }
+
+    private static int digitSum(BigInteger n) {
+        int[] digits = Converter.digitArray(n);
+        int sum = 0;
+
+        for (int digit : digits) sum += digit;
+
+        return sum;
+    }
+}
