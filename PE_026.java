0aa788aefd11e91f0303aa3e68199a4b8d42ac30
LaXHeXLuX <laas.lumberg@gmail.com>
2024-05-17 09:56:34 +0300

    Used CompetitiveCoding Fractions class

--- PE_026.java
+++ PE_026.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class PE_026 {
     public static void main(String[] args) {
@@ -13,7 +11,8 @@ public class PE_026 {
         int longestNumber = 0;
         for (int i = 2; i < limit; i++) {
             int a = 1;
-            int[][] reciprocal = reciprocalCycle(a, i);
+            LongFraction fraction = new LongFraction(a, i);
+            int[][] reciprocal = fraction.getCycle();
             if (reciprocal[1].length > longest) {
                 longest = reciprocal[1].length;
                 longestNumber = i;
@@ -21,43 +20,5 @@ public class PE_026 {
             }
         }
         return longestNumber;
-    }
-
-    private static int[][] reciprocalCycle(int a, int b) {
-        if (a <= 0) throw new RuntimeException("a has to be more than 0");
-        if (b <= 0) throw new RuntimeException("b has to be more than 0");
-        if (b == 1) return new int[][] {};
-        List<Integer> reciprocalCycle = new ArrayList<>();
-        List<Integer> modCycle = new ArrayList<>();
-        int divisible = a;
-
-        while (!ArrayFunctions.contains(divisible, modCycle) && divisible != 0) {
-            modCycle.add(divisible);
-            divisible *= 10;
-            while (divisible < b) {
-                modCycle.add(divisible);
-                divisible *= 10;
-                reciprocalCycle.add(0);
-            }
-            reciprocalCycle.add(divisible / b);
-            divisible = divisible % b;
-        }
-
-        return convertToFraction(divisible, Converter.listToArr(modCycle), Converter.listToArr(reciprocalCycle));
-    }
-
-    private static int[][] convertToFraction(int divisible, int[] modCycle, int[] reciprocalCycle) {
-        int i;
-        for (i = 0; i < modCycle.length; i++) {
-            if (modCycle[i] == divisible) {
-                break;
-            }
-        }
-
-        int[] nonCycle = new int[i];
-        int[] cycle = new int[reciprocalCycle.length-i];
-        System.arraycopy(reciprocalCycle, 0, nonCycle, 0, nonCycle.length);
-        System.arraycopy(reciprocalCycle, i, cycle, 0, cycle.length);
-        return new int[][] {nonCycle, cycle};
     }
 }
