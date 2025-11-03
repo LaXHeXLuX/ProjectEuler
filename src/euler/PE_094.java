52d3eca735fba35b0d291eb8a7f62cb70187ba86
laasl <LaasHendrik.Lumberg@pria.ee>
2025-11-03 15:57:39 +0200

    optimized 094

--- src/euler/PE_094.java
+++ src/euler/PE_094.java
@@ -1,8 +1,5 @@
 package euler;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class PE_094 {
     public static void main(String[] args) {
         System.out.println(PE());
@@ -10,29 +7,24 @@ public class PE_094 {
 
     public static long PE() {
         int limit = 1_000_000_000;
-        long sum = 0;
-        List<Long> perimeters = perimeters(limit);
-        for (long p : perimeters) {
-            sum += p;
-        }
-        return sum;
+        return perimeterSum(limit);
     }
 
-    private static List<Long> perimeters(int perimeterLimit) {
-        List<Long> perimeters = new ArrayList<>();
+    private static long perimeterSum(int perimeterLimit) {
+        long sum = 0;
         int baseLimit = perimeterLimit / 3 / 2;
         long n;
         for (n = 2; n < baseLimit; n++) { // help here
             if (isSquare(3*n*n + 4*n + 1)) {
-                perimeters.add(6*n + 2);
+                sum += 6*n + 2;
             }
             if (isSquare(3*n*n + 2*n)) {
-                perimeters.add(6*n + 4);
+                sum += 6*n + 4;
             }
         }
-        if (isSquare(3*n*n + 4*n + 1)) perimeters.add(6*n + 2);
+        if (isSquare(3*n*n + 4*n + 1)) sum += 6*n + 2;
 
-        return perimeters;
+        return sum;
     }
 
     private static boolean isSquare(long n2) {
