127b8f78ae1d0e4feaf04f595e41c9ab52ec6620
laasl <LaasHendrik.Lumberg@pria.ee>
2025-11-03 11:34:46 +0200

    remove time log

--- src/euler/PE_073.java
+++ src/euler/PE_073.java
@@ -4,10 +4,7 @@ import utils.Divisors;
 
 public class PE_073 {
     public static void main(String[] args) {
-        double s = System.currentTimeMillis();
         System.out.println(PE());
-        double e = System.currentTimeMillis();
-        System.out.println("Time: " + (e-s) + " ms");
     }
 
     public static long PE() {
