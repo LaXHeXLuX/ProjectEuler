575d511a31bf12c2397024cc6909dbdcad63bb16
laasl <LaasHendrik.Lumberg@pria.ee>
2025-11-13 15:46:17 +0200

    made 039 better

--- src/euler/PE_039.java
+++ src/euler/PE_039.java
@@ -1,45 +1,43 @@
 package euler;
 
-import utils.Converter;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class PE_039 {
     public static void main(String[] args) {
+        double s = System.currentTimeMillis();
         System.out.println(PE());
+        double e = System.currentTimeMillis();
+        System.out.println((e-s) + " ms");
     }
 
     public static long PE() {
         int limit = 1_001;
-        return sumWitMostTriangles(limit);
+        return sumWithMostTriangles(limit);
     }
 
     private static boolean isTriangle(int a, int b, int c) {
         return a*a + b*b == c*c;
     }
 
-    private static int[][] findAllTrianglesForSum(int sum) {
-        List<int[]> triangles = new ArrayList<>();
+    private static int triangleCountForPerimeter(int perimeter) {
+        int count = 0;
 
-        for (int a = 1; a < sum/3; a++) {
-            for (int b = a+1; b < sum/2; b++) {
-                int c = sum - a - b;
-                if (isTriangle(a, b, c)) triangles.add(new int[] {a, b, c});
+        for (int a = 1; a < perimeter /3; a++) {
+            for (int b = a+1; b < perimeter /2; b++) {
+                int c = perimeter - a - b;
+                if (isTriangle(a, b, c)) count++;
             }
         }
 
-        return Converter.listToArr(triangles, int[].class);
+        return count;
     }
 
-    private static int sumWitMostTriangles(int limit) {
+    private static int sumWithMostTriangles(int limit) {
         int mostTriangles = 0;
         int bestSum = 0;
 
-        for (int i = 0; i < limit; i++) {
-            int[][] triangles = findAllTrianglesForSum(i);
-            if (triangles.length > mostTriangles) {
-                mostTriangles = triangles.length;
+        for (int i = 0; i < limit; i+=2) {
+            int count = triangleCountForPerimeter(i);
+            if (count > mostTriangles) {
+                mostTriangles = count;
                 bestSum = i;
             }
         }
