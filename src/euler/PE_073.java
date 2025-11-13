0f6f7b1c512170f432398bd8b5d35b7091e39501
laasl <LaasHendrik.Lumberg@pria.ee>
2025-11-13 12:04:28 +0200

    remove time log

--- src/euler/PE_073.java
+++ src/euler/PE_073.java
@@ -6,10 +6,7 @@ import java.util.List;
 public class PE_073 {
     private static final List<List<Integer>> primeFactors = new ArrayList<>();
     public static void main(String[] args) {
-        double s = System.currentTimeMillis();
         System.out.println(PE());
-        double e = System.currentTimeMillis();
-        System.out.println((e-s) + " ms");
     }
 
     public static long PE() {
