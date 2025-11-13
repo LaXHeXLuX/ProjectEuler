f2bf6250018bb79126a9ce6ff5411750d5d02727
laasl <LaasHendrik.Lumberg@pria.ee>
2025-11-13 14:27:10 +0200

    made 058 better

--- src/euler/PE_058.java
+++ src/euler/PE_058.java
@@ -18,20 +18,22 @@ public class PE_058 {
         int d1 = fraction[1];
         int length = 3;
         int primeCounter = 3;
-        while (e1 * (2*length-1) <= primeCounter * d1) {
+        while (e1 * 2*length < primeCounter * d1) {
             length += 2;
-            long[] corners = cornerNumbersForLength(length);
+            long[] corners = primeCornerNumbers(length);
             for (long corner : corners) {
-                if (Primes.isPrime(corner)) primeCounter++;
+                if (Primes.isPrime(corner)) {
+                    primeCounter++;
+                }
             }
         }
         return length;
     }
 
-    private static long[] cornerNumbersForLength(int length) {
-        long lastCorner = (long) (length - 2) * (length-2);
-        long[] corners = new long[4];
-        for (int i = 0; i < 4; i++) {
+    private static long[] primeCornerNumbers(int length) {
+        long lastCorner = (long) (length - 2) * (length - 2);
+        long[] corners = new long[3];
+        for (int i = 0; i < 3; i++) {
             corners[i] = lastCorner + (long) (length - 1) * (i+1);
         }
         return corners;
