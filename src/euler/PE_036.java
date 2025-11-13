c12370c9e160b3b5c15c80ec0423f6aa8920b073
laasl <LaasHendrik.Lumberg@pria.ee>
2025-11-13 11:51:56 +0200

    made 036 better

--- src/euler/PE_036.java
+++ src/euler/PE_036.java
@@ -1,7 +1,5 @@
 package euler;
 
-import utils.Converter;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,35 +11,56 @@ public class PE_036 {
     public static long PE() {
         int[] bases = {2, 10};
         int limit = 1_000_000;
-        long[] palindromes = findPalindromesInBases(limit, bases);
+        List<Long> palindromes = findPalindromesInBases(limit, bases);
 
         long sum = 0;
         for (long palindrome : palindromes) sum += palindrome;
         return sum;
     }
 
-    private static boolean isPalindrome(int[] digitArray) {
-        for (int i = 0; i < digitArray.length/2; i++) {
-            if (digitArray[i] != digitArray[digitArray.length-i-1]) return false;
+    private static boolean isPalindromeInBase(long n, int base) {
+        if (base == 2) {
+            int left = 63 - Long.numberOfLeadingZeros(n);
+            int right = 0;
+            while (right < left) {
+                if (((n >> left) & 1) != ((n >> right) & 1))
+                    return false;
+                left--;
+                right++;
+            }
+            return true;
+        }
+        int[] convertedDigits = new int[(int)(Math.log(n)/Math.log(base))+1];
+        int limit = convertedDigits.length / 2;
+        for (int i = 0; i < limit; i++) {
+            convertedDigits[convertedDigits.length-i-1] = (int) (n%base);
+            n /= base;
+        }
+        if (convertedDigits.length % 2 == 1) n /= base;
+        limit = convertedDigits.length - limit;
+        for (int i = limit; i < convertedDigits.length; i++) {
+            if ((int) (n%base) != convertedDigits[i]) return false;
+            n /= base;
         }
         return true;
     }
 
     private static boolean isPalindromeInBases(long n, int[] bases) {
         for (int base : bases) {
-            int[] baseArray = Converter.convertToBase(n, base);
-            if (!isPalindrome(baseArray)) return false;
+            if (!isPalindromeInBase(n, base)) return false;
         }
         return true;
     }
 
-    private static long[] findPalindromesInBases(long limit, int[] bases) {
+    private static List<Long> findPalindromesInBases(long limit, int[] bases) {
         List<Long> palindromes = new ArrayList<>();
 
-        for (long i = 0; i < limit; i++) {
-            if (isPalindromeInBases(i, bases)) palindromes.add(i);
+        for (long i = 1; i < limit; i++) {
+            if (isPalindromeInBases(i, bases)) {
+                palindromes.add(i);
+            }
         }
 
-        return Converter.listToArr(palindromes);
+        return palindromes;
     }
 }
