37453a4b8a47b164f90875ed68fb12dcbd436d87
laasl <LaasHendrik.Lumberg@pria.ee>
2025-11-13 15:13:00 +0200

    made 048 better

--- src/euler/PE_048.java
+++ src/euler/PE_048.java
@@ -9,40 +9,19 @@ public class PE_048 {
 
     public static long PE() {
         int limit = 1000;
-        int[] arr = arrayOfNFirstDigits(limit);
-        BigInteger answer = selfPowersSum(arr);
+        BigInteger rem = BigInteger.valueOf ((long) Math.pow(10, 10));
+        BigInteger answer = selfPowersSum(limit, rem);
         return answer.remainder(BigInteger.valueOf(10_000_000_000L)).longValue();
     }
 
-    private static int[] arrayOfNFirstDigits(int n) {
-        int[] arr = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = i+1;
-        }
-
-        return arr;
-    }
-
-    private static BigInteger selfPowersSum(int[] numbers) {
+    private static BigInteger selfPowersSum(int limit, BigInteger rem) {
         BigInteger sum = BigInteger.ZERO;
 
-        for (int number : numbers) {
-            BigInteger n = BigInteger.valueOf(number);
-            sum = sum.add(power(n, number));
+        for (int i = 1; i <= limit; i++) {
+            BigInteger n = BigInteger.valueOf(i);
+            sum = sum.add(n.modPow(n, rem)).mod(rem);
         }
 
         return sum;
-    }
-
-    private static BigInteger power(BigInteger n, int power) {
-        power--;
-        BigInteger product = n;
-
-        for (int i = 0; i < power; i++) {
-            product = product.multiply(n);
-        }
-
-        return product;
     }
 }
