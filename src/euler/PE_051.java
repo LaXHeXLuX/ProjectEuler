9a75b4cc080e1060162d94479fabf43aa7acc132
laasl <LaasHendrik.Lumberg@pria.ee>
2025-11-14 09:15:47 +0200

    made 051 better

--- src/euler/PE_051.java
+++ src/euler/PE_051.java
@@ -1,11 +1,9 @@
 package euler;
 
-import utils.Combinations;
 import utils.Converter;
 import utils.Primes;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class PE_051 {
@@ -15,103 +13,102 @@ public class PE_051 {
 
     public static long PE() {
         int size = 8;
-        int[][] first = firstPrimeDigitReplacementFamily(size);
-        return Converter.fromDigitArray(first[0]);
+        int[] first = firstPrimeDigitReplacementFamily(size);
+        List<Integer> score = score(first);
+        return makeNumber(first, score.getFirst());
     }
 
-    private static int[][] firstPrimeDigitReplacementFamily(int familySize) {
-        boolean[] primes = Primes.sieveOfPrimes((int) Math.pow(10, familySize-1));
+    private static int[] firstPrimeDigitReplacementFamily(int familySize) {
         int targetDigitCount = 2;
-        int[][] firstPrimeDigitReplacementFamily = new int[0][];
+        int[] firstPrimeDigitReplacementFamily = new int[0];
+        int injectionAmountStep = 1;
+        if (familySize > 7) injectionAmountStep = 3;
 
-        boolean found = false;
-        while (!found) {
-            int smallestPrimeWithProperty = -1;
-
-            for (int amountOfInjections = 1; amountOfInjections < targetDigitCount; amountOfInjections++) {
-                if (amountOfInjections % 3 != 0) continue;
+        int smallestPrimeWithProperty = -1;
+        while (smallestPrimeWithProperty < 0) {
+            for (int amountOfInjections = injectionAmountStep; amountOfInjections < targetDigitCount; amountOfInjections+=injectionAmountStep) {
                 int amountOfStartingDigits = targetDigitCount-amountOfInjections;
-
-                for (int i = (int) Math.pow(10, amountOfStartingDigits-1); i < Math.pow(10, amountOfStartingDigits); i++) {
+                int limit = (int) Math.pow(10, amountOfStartingDigits);
+                for (int i = limit / 10; i < limit; i++) {
                     int[] digits = Converter.digitArray(i);
-                    int[][] injectionCombinations = Combinations.combinationsOfGrowingNumbers(0, amountOfStartingDigits, amountOfInjections);
-                    int[][] bestInjectionFamily = bestInjectionCombination(injectionCombinations, digits, primes);
-                    if (bestInjectionFamily.length >= familySize) {
-                        int firstNumber = (int) Converter.fromDigitArray(bestInjectionFamily[0]);
-                        found = true;
+                    int[] bestInjection = bestInjectionCombination(digits, amountOfInjections);
+                    List<Integer> score = score(bestInjection);
+                    if (score.size() >= familySize) {
+                        int firstNumber = makeNumber(bestInjection, score.getFirst());
                         if (smallestPrimeWithProperty == -1 || smallestPrimeWithProperty > firstNumber) {
                             smallestPrimeWithProperty = firstNumber;
-                            firstPrimeDigitReplacementFamily = bestInjectionFamily;
+                            firstPrimeDigitReplacementFamily = bestInjection;
                         }
                     }
                 }
             }
-            
             targetDigitCount++;
         }
 
         return firstPrimeDigitReplacementFamily;
     }
 
-    private static int[][] bestInjectionCombination(int[][] injectionCombinations, int[] digits, boolean[] primes) {
-        int smallestPrimeWithProperty = -1;
-        int largestFamilySize = -1;
-        int[][] bestInjectionCombinationFamily = new int[0][];
-
-        for(int[] injectionCombination : injectionCombinations) {
-            int[][] goodInjections = injectionsThatArePrime(digits, injectionCombination, primes);
-            if (goodInjections.length == 0) continue;
-            int firstNumber = (int) Converter.fromDigitArray(goodInjections[0]);
-            int familySize = goodInjections.length;
-            if (familySize > largestFamilySize) {
-                largestFamilySize = familySize;
-                smallestPrimeWithProperty = firstNumber;
-                bestInjectionCombinationFamily = goodInjections;
+    private static int[] injectedNumber(int[] digits, List<Integer> injections) {
+        int[] number = new int[digits.length + injections.size()];
+        int injectionIndex = 0;
+        for (int i = 0; i < digits.length; i++) {
+            while (injectionIndex < injections.size() && injections.get(injectionIndex) == i) {
+                number[i + injectionIndex] = -1;
+                injectionIndex++;
             }
-            else if (smallestPrimeWithProperty == -1 || smallestPrimeWithProperty > firstNumber) {
-                smallestPrimeWithProperty = firstNumber;
-                bestInjectionCombinationFamily = goodInjections;
-            }
+            number[i + injectionIndex] = digits[i];
         }
-
-        return bestInjectionCombinationFamily;
+        return number;
     }
 
-    private static int[][] injectionsThatArePrime(int[] digits, int[] injectionPlaces, boolean[] primes) {
-        List<int[]> injectedNumbersThatArePrime = new ArrayList<>();
+    private static List<Integer> score(int[] number) {
+        List<Integer> score = new ArrayList<>();
         int start = 0;
-        if (injectionPlaces[0] == 0) start = 1;
-
+        if (number[0] == -1) start = 1;
         for (int i = start; i < 10; i++) {
-            int[] injectionPlacesCopy = new int[injectionPlaces.length];
-            System.arraycopy(injectionPlaces, 0, injectionPlacesCopy, 0, injectionPlacesCopy.length);
-            int[] injectedNumber = injectDigit(digits, injectionPlacesCopy, i);
-            if (primes[(int) Converter.fromDigitArray(injectedNumber)]) injectedNumbersThatArePrime.add(injectedNumber);
+            int numberInt = 0;
+            for (int digit : number) {
+                if (digit == -1) digit = i;
+                numberInt = 10 * numberInt + digit;
+            }
+            if (Primes.isPrime(numberInt)) score.add(i);
         }
-
-        return Converter.listToArr(injectedNumbersThatArePrime, int[].class);
+        return score;
     }
 
-    private static int[] injectDigit(int[] digits, int[] injectionPlaces, int injectableDigit) {
-        int[] newDigits = new int[digits.length + injectionPlaces.length];
-        Arrays.fill(newDigits, -1);
+    private static int makeNumber(int[] number, int digit) {
+        int result = 0;
+        for (int i : number) {
+            int d;
+            if (i == -1) d = digit;
+            else d = i;
+            result = result * 10 + d;
+        }
+        return result;
+    }
 
-        int newDigitIndex;
-        for (int i = 0; i < injectionPlaces.length; i++) {
-            newDigitIndex = injectionPlaces[i];
-            newDigits[newDigitIndex] = injectableDigit;
-            for (int j = i+1; j < injectionPlaces.length; j++) {
-                injectionPlaces[j]++;
+    private static int[] bestInjectionCombination(int[] digits, int n) {
+        return bestInjectionCombination(digits, n, new ArrayList<>());
+    }
+
+    private static int[] bestInjectionCombination(int[] digits, int n, List<Integer> injections) {
+        if (n == 0) {
+            return injectedNumber(digits, injections);
+        }
+        int[] bestNumber = null;
+        int bestScore = -1;
+        int start = 0;
+        if (!injections.isEmpty()) start = injections.getLast();
+        for (int i = start; i < digits.length; i++) {
+            injections.add(i);
+            int[] number = bestInjectionCombination(digits, n-1, injections);
+            injections.removeLast();
+            List<Integer> score = score(number);
+            if (score.size() > bestScore) {
+                bestScore = score.size();
+                bestNumber = number;
             }
         }
-
-        int digitIndex = 0;
-        for (int i = 0; i < newDigits.length; i++) {
-            if (newDigits[i] != -1) continue;
-            newDigits[i] = digits[digitIndex];
-            digitIndex++;
-        }
-
-        return newDigits;
+        return bestNumber;
     }
 }
