32451b290b28ab4bad50113f2c496dd77b01e7d2
LaXHeXLuX <laas.lumberg@gmail.com>
2025-11-14 19:46:46 +0200

    Added 107

---
+++ src/euler/PE_107.java
@@ -0,0 +1,43 @@
+package euler;
+
+import utils.Graph;
+import utils.Parser;
+
+import java.util.Set;
+
+public class PE_107 {
+    public static void main(String[] args) {
+        System.out.println(PE());
+    }
+
+    public static long PE() {
+        String filename = "src/euler/inputs/PE_107_network.txt";
+        Graph graph = parser(filename);
+        int sum1 = edgeSum(graph);
+        Graph mst = graph.mst();
+        int sum2 = edgeSum(mst);
+        return sum1-sum2;
+    }
+    
+    private static int edgeSum(Graph graph) {
+        int sum = 0;
+        Set<Graph.Edge> edges = graph.getEdges();
+        for (Graph.Edge edge : edges) {
+            sum += edge.getWeight();
+        }
+        return sum / 2;
+    }
+
+    private static Graph parser(String filename) {
+        String[][] cells = Parser.parseManyStrings(filename, ",");
+        int[][] edgeMatrix = new int[cells.length][];
+        for (int i = 0; i < cells.length; i++) {
+            edgeMatrix[i] = new int[cells[i].length];
+            for (int j = 0; j < cells[i].length; j++) {
+                if (cells[i][j].equals("-")) edgeMatrix[i][j] = -1;
+                else edgeMatrix[i][j] = Integer.parseInt(cells[i][j]);
+            }
+        }
+        return new Graph(edgeMatrix);
+    }
+}
