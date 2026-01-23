927833ab888a8fb784d57a1033aee61512ea3318
LaXHeXLuX <laas.lumberg@gmail.com>
2026-01-23 16:38:41 +0200

    outsource modDivide

--- src/euler/PE_134.java
+++ src/euler/PE_134.java
@@ -27,23 +27,11 @@ public class PE_134 {
         return sum;
     }
 
-    private static int modulusMultiplier(int a, int b, int c) {
-        int d = Math.toIntExact(Diophantine.gcd(a, c));
-        if (b % d != 0) return -1;
-        a /= d;
-        b /= d;
-        c /= d;
-        long[] sol = Diophantine.extendedEuclidean(a, c);
-        int r = Math.toIntExact((sol[0] * b) % c);
-        if (r < 0) r += c;
-        return r;
-    }
-
     private static long smallestNForP1AndP2(int p1, int p2) {
         int p1Size = (int) Math.log10(p1) + 1;
         int p1Mod = Math.toIntExact(Diophantine.powMod(10, p1Size, p2));
         int diff = p2 - p1;
-        long n = modulusMultiplier(p1Mod, diff, p2);
+        long n = Diophantine.modDivide(p1Mod, diff, p2);
         if (n == -1) return -1;
         for (int i = 0; i < p1Size; i++) n *= 10;
         return n + p1;
