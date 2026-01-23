2fb48b97237d4f2831e19045985129c1df964306
LaXHeXLuX <laas.lumberg@gmail.com>
2026-01-24 00:33:52 +0200

    remove unused import

--- tests/utils/DiophantineTest.java
+++ tests/utils/DiophantineTest.java
@@ -3,7 +3,6 @@ package utils;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
