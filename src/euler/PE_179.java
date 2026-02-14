93ab476495d4c45123ef6f6cdc79fba65879f8e9
LaXHeXLuX <laas.lumberg@gmail.com>
2026-02-14 14:40:27 +0200

    remove time logs

--- src/euler/PE_179.java
+++ src/euler/PE_179.java
@@ -4,10 +4,7 @@ import java.util.Arrays;
 
 public class PE_179 {
     public static void main(String[] args) {
-        double s = System.currentTimeMillis();
         System.out.println(PE());
-        double e = System.currentTimeMillis();
-        System.out.println((e - s) + " ms");
     }
 
     public static long PE() {
