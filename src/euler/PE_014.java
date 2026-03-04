422d971471141f921e0b6565ffff735276360514
laasl <LaasHendrik.Lumberg@pria.ee>
2026-03-04 14:27:06 +0200

    made 014 (a bit) better

--- src/euler/PE_014.java
+++ src/euler/PE_014.java
@@ -2,6 +2,7 @@ package euler;
 
 public class PE_014 {
     private static boolean[] skip;
+    private static int[] firstCollatz;
 
     static void main() {
         System.out.println(PE());
@@ -9,16 +10,24 @@ public class PE_014 {
 
     public static String PE() {
         int limit = 1_000_000;
+        makeFirstCollatz(limit);
         return String.valueOf(longestCollatzChainLength(limit));
+    }
+
+    private static void makeFirstCollatz(int limit) {
+        int size = limit/100;
+        firstCollatz = new int[size];
+        for (int i = 1; i < size; i++) {
+            firstCollatz[i] = collatzLength(i);
+        }
     }
 
     private static long longestCollatzChainLength(int limit) {
         long biggestChainLength = 0;
         long biggestI = 0;
         skip = new boolean[limit];
-        int start = limit/2 - 1;
-        for (int i = start; i < limit; i++) {
-            if (skip[i] || (i % 3 == 1 && i % 2 == 0)) continue;
+        for (int i = limit/2; i < limit; i++) {
+            if (skip[i] || (i % 6 == 4)) continue;
             long chainLength = collatzChainLength(i);
             if (chainLength > biggestChainLength) {
                 biggestChainLength = chainLength;
@@ -28,17 +37,52 @@ public class PE_014 {
         return biggestI;
     }
 
-    private static int collatzChainLength(int n) {
-        int chainLength = 1;
+    private static int collatzLength(long n) {
+        int c = 1;
         while (n > 1) {
-            if (n < skip.length) skip[n] = true;
             if ((n & 1) == 0) n = n >> 1;
-            else {
-                n = 3*(n >> 1) + 2;
-                chainLength++;
-            }
-            chainLength++;
+            else n = 3*n + 1;
+            c++;
         }
-        return chainLength;
+        return c;
+    }
+
+    private static int collatzChainLength(long n) {
+        int chainLength = 0;
+        while (n >= firstCollatz.length) {
+            if (n < skip.length) skip[(int) n] = true;
+            int mod = (int) (n & 7);
+            switch (mod) {
+                case 0 -> {
+                    n = n >> 3;
+                    chainLength += 3;
+                }
+                case 1 -> {
+                    n = 9*(n >> 3) + 2;
+                    chainLength += 5;
+                }
+                case 2 -> {
+                    n = 3*(n >> 3) + 1;
+                    chainLength += 4;
+                }
+                case 3 -> {
+                    n = 9*(n >> 3) + 4;
+                    chainLength += 5;
+                }
+                case 4, 5 -> {
+                    n = 3*(n >> 3) + 2;
+                    chainLength += 4;
+                }
+                case 6 -> {
+                    n = 9*(n >> 3) + 8;
+                    chainLength += 5;
+                }
+                case 7 -> {
+                    n = 27*(n >> 3) + 26;
+                    chainLength += 6;
+                }
+            }
+        }
+        return chainLength + firstCollatz[(int) n];
     }
 }
