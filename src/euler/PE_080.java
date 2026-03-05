422098052b99e42593d0be71a67b258e645a9f6a
laasl <LaasHendrik.Lumberg@pria.ee>
2026-03-05 14:23:36 +0200

    made 080 better

--- src/euler/PE_080.java
+++ src/euler/PE_080.java
@@ -15,45 +15,17 @@ public class PE_080 {
         return String.valueOf(totalOfDigitSums(limit, precision));
     }
 
-    private static boolean isSquare(long n) {
-        long root = (long) Math.sqrt(n);
-        return root * root == n;
-    }
-
     private static int totalOfDigitSums(int limit, int precision) {
         int total = 0;
-
         for (int i = 0; i <= limit; i++) {
-            if (isSquare(i)) continue;
-            total += digitSumOfSquareRootOf(i, precision);
+            if (Diophantine.root(i) >= 0) continue;
+            total += digitSumOfSquareRoot(i, precision);
         }
-
         return total;
     }
 
-    private static int digitSumOfSquareRootOf(int n, int precision) {
-        BigInteger squareRoot = squareRoot(n, precision);
-        BigInteger power10 = BigInteger.TEN.pow(precision);
-        while (squareRoot.compareTo(power10) >= 0) {
-            squareRoot = squareRoot.divide(BigInteger.TEN);
-        }
-        return Diophantine.digitSum(squareRoot);
-    }
-
-    private static BigInteger squareRoot(int n, int precision) {
-        BigInteger number = BigInteger.valueOf(n).multiply(BigInteger.TEN.pow(precision * 2));
-        BigInteger old = number;
-        BigInteger root = heron(old, number);
-
-        while (!root.equals(old)) {
-            old = root;
-            root = heron(old, number);
-        }
-
-        return root;
-    }
-
-    private static BigInteger heron(BigInteger x, BigInteger a) {
-        return x.add(a.divide(x)).divide(BigInteger.TWO);
+    private static int digitSumOfSquareRoot(int n, int precision) {
+        BigInteger sqrt = BigInteger.valueOf(n).multiply(BigInteger.TEN.pow(2*(precision-1))).sqrt();
+        return Diophantine.digitSum(sqrt);
     }
 }
