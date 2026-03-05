e3ccf82cd39340c0a9624a635c5931e93a6c10cc
laasl <LaasHendrik.Lumberg@pria.ee>
2026-03-05 10:52:46 +0200

    refactored 058 (not really better)

--- src/euler/PE_058.java
+++ src/euler/PE_058.java
@@ -2,10 +2,15 @@ package euler;
 
 import utils.Primes;
 
+import java.util.Arrays;
+
 public class PE_058 {
 
     static void main() {
+        double s = System.currentTimeMillis();
         System.out.println(PE());
+        double e = System.currentTimeMillis();
+        System.out.println((e - s) + " ms");
     }
 
     public static String PE() {
@@ -14,28 +19,17 @@ public class PE_058 {
     }
 
     private static int firstSpiralLengthWithPrimeDiagonalRatioBelow(int[] fraction) {
-        int e1 = fraction[0];
-        int d1 = fraction[1];
-        int length = 3;
+        int num = fraction[0];
+        int den = fraction[1];
+        int l = 3;
         int primeCounter = 3;
-        while (e1 * 2*length < primeCounter * d1) {
-            length += 2;
-            long[] corners = primeCornerNumbers(length);
-            for (long corner : corners) {
-                if (Primes.isPrime(corner)) {
-                    primeCounter++;
-                }
-            }
+        while (num * 2*l < primeCounter * den) {
+            l += 2;
+            int ll = l*l;
+            if (l % 3 != 0 && Primes.isPrime(ll - 3L*l + 3)) primeCounter++;
+            if (Primes.isPrime(ll - 2L*l + 2)) primeCounter++;
+            if (l % 3 != 2 && Primes.isPrime(ll - l + 1)) primeCounter++;
         }
-        return length;
-    }
-
-    private static long[] primeCornerNumbers(int length) {
-        long lastCorner = (long) (length - 2) * (length - 2);
-        long[] corners = new long[3];
-        for (int i = 0; i < 3; i++) {
-            corners[i] = lastCorner + (long) (length - 1) * (i+1);
-        }
-        return corners;
+        return l;
     }
 }
