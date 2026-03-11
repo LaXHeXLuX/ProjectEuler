43c43a36659d105def128f496e296b7f36bebbac
laasl <LaasHendrik.Lumberg@pria.ee>
2026-03-11 15:16:51 +0200

    made 357 (a bit) better

--- src/euler/PE_357.java
+++ src/euler/PE_357.java
@@ -6,51 +6,30 @@ public class PE_357 {
     private static boolean[] composites;
 
     static void main() {
-        double s = System.currentTimeMillis();
         System.out.println(PE());
-        double e = System.currentTimeMillis();
-        System.out.println((e-s) + " ms");
     }
 
     public static String PE() {
         int limit = 100_000_000;
-        double s = System.currentTimeMillis();
         composites = Primes.compositeSieve(limit+1);
-        double e = System.currentTimeMillis();
-        System.out.println("composites " + (e-s) + " ms");
         return String.valueOf(sumOfAll(limit));
     }
 
     private static long sumOfAll(int limit) {
-        long sum = 3;
-        int b = 60;
-        int[] arr = {10, 18, 22, 30, 42, 58};
+        long sum = 1+2+6;
 
-        for (int i = 6; i < b; i+=4) {
-            if (i > limit) break;
+        for (int i = 10; i < limit; i+=4) {
+            if (composites[i/2] || composites[i/4 + 1]) continue;
             if (primeGenerating(i)) sum += i;
-        }
-
-        int iLimit = limit/b;
-        for (int i = 1; i <= iLimit; i++) {
-            int base = i*b;
-            for (int j : arr) {
-                int n = base + j;
-                if (n > limit) break;
-                if (primeGenerating(n)) sum += n;
-            }
         }
 
         return sum;
     }
 
     private static boolean primeGenerating(int n) {
-        if (composites[(n+1)/2] || composites[(n/2+2)/2]) return false;
-        int d;
-        for (d = 3; d*d <= n; d++) {
-            if (n % d != 0) continue;
-            if (composites[(d + n/d)/2]) break;
+        for (int d = 3; d*d <= n; d++) {
+            if (n % d == 0 && composites[(d + n/d)/2]) return false;
         }
-        return d*d > n;
+        return true;
     }
 }
