a2f027d61090bd086c75bae3ac0e3d7106121805
LaXHeXLuX <laas.lumberg@gmail.com>
2026-03-13 13:33:37 +0200

    made 381 (a bit) better

--- src/euler/PE_381.java
+++ src/euler/PE_381.java
@@ -5,16 +5,13 @@ import utils.Primes;
 
 public class PE_381 {
     static void main() {
-        double s = System.currentTimeMillis();
         System.out.println(PE());
-        double e = System.currentTimeMillis();
-        System.out.println((e - s) + " ms");
     }
 
     public static String PE() {
         int[] primes = Primes.primes(100_000_000);
-        long sum = 0;
-        for (int i = 2; i < primes.length; i++) {
+        long sum = 4+4+1+11+6+2+14;
+        for (int i = 9; i < primes.length; i++) {
             int p = primes[i];
             sum += S(p);
         }
@@ -22,12 +19,10 @@ public class PE_381 {
     }
 
     private static int S(int p) {
-        int a = (int) (((((p-2L)*(p-3)) % p) * (p-4)) % p);
-        int P5 = modDivide(a, p);
-        return (9 * P5) % p;
+        return (9 * modDivide(p-24, p)) % p;
     }
 
-    public static int modDivide(int a, int mod) {
+    private static int modDivide(int a, int mod) {
         int[] sol = Diophantine.extendedEuclidean(a, mod);
         int result = sol[0] % mod;
         if (result < 0) result += mod;
