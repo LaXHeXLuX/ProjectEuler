d7c56df145d7ea9d350fefdbc31b37222d53ad75
LaXHeXLuX <laas.lumberg@gmail.com>
2026-03-18 21:48:43 +0200

    made 016 better

--- src/euler/PE_016.java
+++ src/euler/PE_016.java
@@ -12,15 +12,7 @@ public class PE_016 {
     public static String PE() {
         int n = 2;
         int p = 1000;
-        return String.valueOf(Diophantine.digitSum(power(n, p)));
-    }
-
-    private static BigInteger power(int n, int power) {
-        BigInteger startingNumber = BigInteger.valueOf(n);
-        BigInteger answer = startingNumber;
-        for (int i = 1; i < power; i++) {
-            answer = answer.multiply(startingNumber);
-        }
-        return answer;
+        BigInteger power = BigInteger.valueOf(n).pow(p);
+        return String.valueOf(Diophantine.digitSum(power));
     }
 }
