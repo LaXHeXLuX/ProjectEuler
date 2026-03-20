3a4a3968a1199b8312ec5f30a29a2f789d771008
LaXHeXLuX <laas.lumberg@gmail.com>
2026-03-20 09:10:28 +0200

    make 029 better

--- src/euler/PE_029.java
+++ src/euler/PE_029.java
@@ -1,18 +1,15 @@
 package euler;
 
-import utils.Diophantine;
 import utils.Primes;
 
-import java.math.BigInteger;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 public class PE_029 {
+    private static int[] pow;
+    private static int[] largestFactor;
+
     static void main() {
-        double s = System.currentTimeMillis();
         System.out.println(PE());
-        double e = System.currentTimeMillis();
-        System.out.println((e-s) + " ms");
     }
 
     public static String PE() {
@@ -20,35 +17,63 @@ public class PE_029 {
         return String.valueOf(distinctPowerCount(limit, limit));
     }
 
+    private static void makePow(int n) {
+        pow = new int[n+1];
+        Arrays.fill(pow, 1);
+        int limit = (int) Math.sqrt(n);
+        for (int i = 2; i <= limit; i++) {
+            if (pow[i] > 1) continue;
+            long p = (long) i * i;
+            int s = 2;
+            while (p < pow.length) {
+                pow[Math.toIntExact(p)] = s;
+                p *= i;
+                s++;
+            }
+        }
+    }
+
+    private static void makeLargestFactor(int n) {
+        int limit = (int) (Math.log(n) / Math.log(2));
+        largestFactor = new int[limit+1];
+        largestFactor[1] = 1;
+        for (int i = 2; i < largestFactor.length; i++) {
+            Primes.PF[] pfs = Primes.primeFactors(i);
+            largestFactor[i] = Math.toIntExact(pfs[pfs.length-1].primeFactor);
+        }
+    }
+
     private static long distinctPowerCount(int limitA, int limitB) {
+        makePow(limitA);
+        makeLargestFactor(limitA);
         boolean[] primes = Primes.sieve(limitB+1);
         primes[2] = false;
         long counter = 0;
         for (int a = 2; a <= limitA; a++) {
-            int add;
-            Primes.PF[] pfs = Primes.primeFactors(a);
-            int gcd = gcd(pfs);
-            int gcdSmallestDivisor = 1;
-            if (gcd > 1) gcdSmallestDivisor = Math.toIntExact(Primes.primeFactors(gcd)[0].primeFactor);
-            int gcdLargestDivisor = gcdSmallestDivisor;
-            if (gcdSmallestDivisor != gcd) gcdLargestDivisor = gcd / gcdSmallestDivisor;
-            if (gcd == 1) {
-                add = limitB-1;
-            }
-            else {
-                int start = limitB/gcdLargestDivisor + 1;
-                if (start < 2) start = 2;
-                int end = (gcd-1)*limitB/gcd;
-                add = limitB - end;
-                for (int b = start; b <= end; b++) {
-                    int div = biggestDivisorSmallerThan(b*gcd, gcd);
-                    if (b * gcd / div > limitB) add++;
-                }
-            }
-            counter += add;
-            //System.out.println("a: " + a + ", gcd: " + gcd + ", smallest: " + gcdSmallestDivisor + ", largest: " + gcdLargestDivisor + ", adding: " + add);
+            counter += add(limitB, a);
         }
         return counter;
+    }
+
+    private static int add(int limitB, int a) {
+        int add;
+        int gcd = pow[a];
+        int gcdLargestDivisor = largestFactor[gcd];
+
+        if (gcd == 1) {
+            add = limitB -1;
+        }
+        else {
+            int start = limitB /gcdLargestDivisor + 1;
+            if (start < 2) start = 2;
+            int end = (gcd-1)* limitB /gcd;
+            add = limitB - end;
+            for (int b = start; b <= end; b++) {
+                int div = biggestDivisorSmallerThan(b*gcd, gcd);
+                if (b * gcd / div > limitB) add++;
+            }
+        }
+        return add;
     }
 
     private static int biggestDivisorSmallerThan(int n, int limit) {
@@ -56,31 +81,5 @@ public class PE_029 {
         int i = limit-1;
         while (n % i != 0) i--;
         return i;
-    }
-
-    private static int gcd(Primes.PF[] pfs) {
-        int gcd = pfs[0].power;
-        for (int i = 1; i < pfs.length; i++) {
-            gcd = Diophantine.gcd(gcd, pfs[i].power);
-        }
-        return gcd;
-    }
-
-    private static long distinctPowerCount0(int limitA, int limitB) {
-        Set<BigInteger> powers = new HashSet<>();
-        long counter = 0;
-        for (int a = 2; a <= limitA; a++) {
-            int add = 0;
-            for (int b = 2; b <= limitB; b++) {
-                BigInteger bi = BigInteger.valueOf(a).pow(b);
-                if (!powers.contains(bi)) {
-                    add++;
-                    powers.add(bi);
-                }
-            }
-            System.out.println("a: " + a + ", adding " + add);
-            counter += add;
-        }
-        return counter;
     }
 }
