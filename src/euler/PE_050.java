705ffe5c8f029501c4453dba2ec654f30aa29b7a
LaXHeXLuX <laas.lumberg@gmail.com>
2026-03-23 12:32:01 +0200

    made 050 (slightly) better

--- src/euler/PE_050.java
+++ src/euler/PE_050.java
@@ -5,50 +5,44 @@ import utils.Primes;
 public class PE_050 {
     private static boolean[] composites;
     private static int[] primes;
+
     static void main() {
         System.out.println(PE());
     }
 
     public static String PE() {
         int limit = 1_000_000;
+
         composites = Primes.compositeSieve(limit);
         primes = Primes.primes(composites);
 
-        int result = sumOfConsecutivePrimesSummingToPrime();
+        int result = sumOfConsecutivePrimesSummingToPrime2();
         return String.valueOf(result);
     }
 
-    private static int sumOfConsecutivePrimesSummingToPrime() {
-        int largestLength = 0;
-        int largestSum = 0;
-        for (int startIndex = 0; ; startIndex++) {
-            if (startIndex + largestLength > composites.length) break;
-            int sum = 0;
-            boolean flag = true;
-            int i;
-            for (i = 0; i < largestLength; i++) {
-                if (startIndex+i >= primes.length) {
-                    flag = false;
-                    break;
-                }
-                sum += primes[startIndex + i];
-                if (sum >= primes[primes.length-1]) {
-                    flag = false;
-                    break;
-                }
-            }
-            if (!flag) continue;
-            sum += primes[startIndex + i];
-            while (sum <= primes[primes.length-1]) {
-                if (!composites[sum >> 1]) {
-                    largestLength = i;
-                    largestSum = sum;
-                }
-                i+=2;
-                sum += primes[startIndex + i - 1] + primes[startIndex + i];
-            }
-            if (i < largestLength) break;
+    private static int sumOfConsecutivePrimesSummingToPrime2() {
+        long[] sums = new long[primes.length + 1];
+        for (int i = 0; i < primes.length; i++) {
+            sums[i + 1] = sums[i] + primes[i];
         }
-        return largestSum;
+
+        int bestStart = 0;
+        int bestLength = 0;
+        int limit = primes[primes.length - 1];
+
+        for (int i = 0; i < primes.length; i++) {
+            if (sums[i] + bestLength + 1 > limit) break;
+
+            for (int end = i + bestLength + 1; end <= primes.length; end++) {
+                long sum = sums[end] - sums[i];
+                if (sum > limit) break;
+                if (!composites[(int) sum >> 1]) {
+                    bestStart = i;
+                    bestLength = end - i;
+                }
+            }
+        }
+
+        return (int) (sums[bestStart + bestLength] - sums[bestStart]);
     }
 }
