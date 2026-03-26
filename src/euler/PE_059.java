a0da23809e8a2502efe8fbdd5efe1204377d2adb
LaXHeXLuX <laas.lumberg@gmail.com>
2026-03-26 16:52:00 +0200

    made 059 (a bit) better

--- src/euler/PE_059.java
+++ src/euler/PE_059.java
@@ -1,9 +1,9 @@
 package euler;
 
-import utils.Converter;
 import utils.Parser;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PE_059 {
     static void main() {
@@ -38,29 +38,24 @@ public class PE_059 {
     }
 
     private static int[][] divideIntoThree(int[] chars) {
-        List<Integer> arr1 = new ArrayList<>();
-        List<Integer> arr2 = new ArrayList<>();
-        List<Integer> arr3 = new ArrayList<>();
-        List<List<Integer>> allLists = new ArrayList<>();
-        allLists.add(arr1);
-        allLists.add(arr2);
-        allLists.add(arr3);
-
+        int[][] charsDivided = new int[3][];
+        for (int i = 0; i < 3; i++) {
+            int l = chars.length/3;
+            if (chars.length % 3 > i) l++;
+            charsDivided[i] = new int[l];
+        }
         for (int i = 0; i < chars.length; i++) {
-            allLists.get(i%3).add(chars[i]);
+            charsDivided[i%3][i/3] = chars[i];
         }
 
-        int[] chars1 = Converter.listToArr(allLists.get(0));
-        int[] chars2 = Converter.listToArr(allLists.get(1));
-        int[] chars3 = Converter.listToArr(allLists.get(2));
-        return new int[][] {chars1, chars2, chars3};
+        return charsDivided;
     }
 
     private static int[] likelyKey(int[] chars) {
         int[][] threeSets = divideIntoThree(chars);
         int[] likelyKey = new int[3];
         for (int i = 0; i < threeSets.length; i++) {
-            likelyKey[i] = XOR(mostCommonElement(threeSets[i]), ' ');
+            likelyKey[i] = mostCommonElement(threeSets[i]) ^ (int) ' ';
         }
 
         return likelyKey;
@@ -81,52 +76,9 @@ public class PE_059 {
 
         for (int i = 0; i < output.length; i++) {
             int keyIndex = i % key.length;
-            output[i] = XOR(input[i], key[keyIndex]);
+            output[i] = input[i] ^ key[keyIndex];
         }
 
         return output;
-    }
-
-    private static int XOR(int input, int key) {
-        boolean[] inputBase2 = Converter.booleanConversion(Converter.convertToBase(input, 2));
-        boolean[] keyBase2 = Converter.booleanConversion(Converter.convertToBase(key, 2));
-        boolean[] outputBase2 = XOR(inputBase2, keyBase2);
-        return (int) Converter.convertFromBase(Converter.booleanConversion(outputBase2), 2);
-    }
-
-    private static boolean[] XOR(boolean[] input, boolean[] key) {
-        if (input.length != key.length) {
-            boolean[][] inputs = assertSameSize(input, key);
-            input = inputs[0];
-            key = inputs[1];
-        }
-        boolean[] output = new boolean[input.length];
-
-        for (int i = output.length-1; i >= 0; i--) {
-            int keyIndex = i % key.length;
-            output[i] = XOR(input[i], key[keyIndex]);
-        }
-
-        return output;
-    }
-
-    private static boolean XOR(boolean a, boolean b) {
-        return (!a && b) || (a && !b);
-    }
-
-    private static boolean[][] assertSameSize(boolean[] arr1, boolean[] arr2) {
-        if (arr1.length == arr2.length) return new boolean[][] {arr1, arr2};
-        if (arr1.length > arr2.length) {
-            arr2 = addZeroesToFront(arr2, arr1.length);
-            return new boolean[][] {arr1, arr2};
-        }
-        arr1 = addZeroesToFront(arr1, arr2.length);
-        return new boolean[][] {arr1, arr2};
-    }
-
-    private static boolean[] addZeroesToFront(boolean[] arr, int size) {
-        boolean[] newArr = new boolean[size];
-        System.arraycopy(arr, 0, newArr, newArr.length-arr.length, arr.length);
-        return newArr;
     }
 }
