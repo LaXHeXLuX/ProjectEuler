777204d06268693d575c262f91eec99bcf939752
LaXHeXLuX <laas.lumberg@gmail.com>
2026-03-28 08:01:28 +0200

    made 071 better

--- src/euler/PE_071.java
+++ src/euler/PE_071.java
@@ -13,26 +13,26 @@ public class PE_071 {
         return String.valueOf(fractionBefore(limitD, fraction)[0]);
     }
 
-    private static int numerator(int[] fraction, int d) {
-        if (fraction[1] == d) return fraction[0]-1;
-        return fraction[0]*d/fraction[1];
-    }
-
     private static int[] fractionBefore(int limitD, int[] fraction) {
-        int[] winningFraction = {0, 1};
-        for (int d = 2; d <= limitD; d++) {
-            if (d % fraction[1] == 0) continue;
-            int[] currentFraction = {numerator(fraction, d), d};
-
-            if (compare(currentFraction, winningFraction) > 0) winningFraction = currentFraction;
+        int[] winningFraction = {-1, 1};
+        long[] winningDiff = {1, 0};
+        long N = fraction[0];
+        long D = fraction[1];
+        for (long d = limitD; d > 2; d--) {
+            if (d % D == 0) continue;
+            long n = (N*d)/D;
+            if (Diophantine.gcd(n, d) > 1) continue;
+            long diffN = N*d - n*D;
+            if (diffN == 1) return new int[] {(int) n, (int) d};
+            long diffD = D*d;
+            if (diffN*winningDiff[1] < diffD*winningDiff[0]) {
+                winningDiff[0] = diffN;
+                winningDiff[1] = diffD;
+                winningFraction[0] = (int) n;
+                winningFraction[1] = (int) d;
+            }
         }
-        return winningFraction;
-    }
 
-    private static int compare(int[] fraction1, int[] fraction2) {
-        int gcd = Diophantine.gcd(fraction1[1], fraction2[1]);
-        int numerator1 = fraction1[0] * (fraction2[1]/gcd);
-        int numerator2 = fraction2[0] * (fraction1[1]/gcd);
-        return Integer.compare(numerator1, numerator2);
+        return winningFraction;
     }
 }
