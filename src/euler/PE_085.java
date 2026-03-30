d39c4196e399e1e7c030815ec089e3c3bd7fa19e
LaXHeXLuX <laas.lumberg@gmail.com>
2026-03-30 23:38:10 +0300

    made 085 better

--- src/euler/PE_085.java
+++ src/euler/PE_085.java
@@ -12,58 +12,34 @@ public class PE_085 {
     }
 
     private static int[] closestForLimit(long limit) {
-        int closestFor1 = limitFor(1, limit);
+        int closestFor1 = limitFor2(1, limit);
         int[] closest = {1, closestFor1};
         long closestDifference = Math.abs(rectangleCount(1, closestFor1) - limit);
 
-        for (int x = 2; x <= closestFor1; x++) {
-            if (rectangleCount(x, x+1) < limit)
-                continue;
-
-            int closestY = limitFor(x, limit);
-            long count = rectangleCount(x, closestY);
+        for (int m = 2; m <= closestFor1; m++) {
+            int closestN = limitFor2(m, limit);
+            long count = rectangleCount(m, closestN);
 
             if (Math.abs(count - limit) < closestDifference) {
                 closestDifference = Math.abs(count - limit);
-                closest = new int[] {x, closestY};
+                closest = new int[] {m, closestN};
             }
         }
 
         return closest;
     }
 
-    private static int limitFor(int x, long limit) {
-        int y = 0;
-        long count = rectangleCount(x, y);
-        long lastCount = count;
-
-        while (count < limit) {
-            lastCount = count;
-            y++;
-            count = rectangleCount(x, y);
-        }
-
-        if (count - limit > limit - lastCount)
-            y--;
-
-        return y;
+    private static int limitFor2(int m, long limit) {
+        int n1 = (int) Math.sqrt((double) (limit << 2) / (m*(m+1)));
+        int n2 = n1 +1;
+        long diff1 = limit - rectangleCount(m, n1);
+        long diff2 = rectangleCount(m, n2) - limit;
+        int result = n1;
+        if (diff1 > diff2) result = n2;
+        return result;
     }
 
-    private static long rectangleCount(int x, int y) {
-        if (x > y) {
-            int[] swap = {y, x};
-            x = swap[0];
-            y = swap[1];
-        }
-        return rectangleCountInGrid(x, y);
-    }
-
-    private static long rectangleCountInGrid(int x, int y) {
-        if (x <= 0)
-            return 0;
-        if (x == 1)
-            return (long) y * (y + 1) / 2;
-
-        return (long) y * x * (y+1) * (x+1) / 4;
+    private static long rectangleCount(int m, int n) {
+        return ((long) n * m * (n+1) * (m+1)) >> 2;
     }
 }
