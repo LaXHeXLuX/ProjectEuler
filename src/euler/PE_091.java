1128974347fa86e7e5ead6a12c0caf2131803f0c
laasl <LaasHendrik.Lumberg@pria.ee>
2026-03-31 11:43:51 +0300

    made 091 better

--- src/euler/PE_091.java
+++ src/euler/PE_091.java
@@ -1,5 +1,7 @@
 package euler;
 
+import utils.Diophantine;
+
 public class PE_091 {
     static void main() {
         System.out.println(PE());
@@ -7,36 +9,33 @@ public class PE_091 {
 
     public static String PE() {
         int n = 50;
-        return String.valueOf(rightTrianglesBelow(n));
+        return String.valueOf(rightTrianglesBelow2(n));
     }
 
-    private static long rightTrianglesBelow(int n) {
-        long count = 0;
-        for (int x1 = 0; x1 <= n; x1++) {
-            for (int y1 = 0; y1 <= n; y1++) {
-                if (x1 == 0 && y1 == 0) continue;
-                for (int x2 = 0; x2 <= x1; x2++) {
+    private static long rightTrianglesBelow2(int n) {
+        long count = 3L*n*n;
+        for (int x2 = 1; x2 <= n; x2++) {
+            for (int x1 = 1; x1 < x2; x1++) {
+                int y2 = Diophantine.root(x1*(x2-x1));
+                if (y2 > 0) count += 2;
+            }
+        }
+        for (int x1 = 1; x1 <= n; x1++) {
+            for (int x2 = 1; x2 <= x1; x2++) {
+                int xd = x2-x1;
+                for (int y1 = 1; y1 <= n; y1++) {
                     int y2Limit = n;
                     if (x1 == x2) y2Limit = y1;
-                    for (int y2 = 0; y2 <= y2Limit; y2++) {
-                        if (x2 == 0 && y2 == 0) continue;
+                    for (int y2 = 1; y2 <= y2Limit; y2++) {
                         if (x1 == x2 && y1 == y2) continue;
-                        int[] OP = {x1, y1};
-                        int[] PQ = {x2-x1, y2-y1};
-                        int[] QO = {-x2, -y2};
+                        int yd = y2-y1;
                         if (
-                            dot(OP, PQ) == 0 ||
-                            dot(PQ, QO) == 0 ||
-                            dot(QO, OP) == 0
-                        ) count++;
+                            x1*xd + y1*yd == 0
+                        ) count += 2;
                     }
                 }
             }
         }
         return count;
-    }
-
-    private static int dot(int[] v1, int[] v2) {
-        return v1[0] * v2[0] + v1[1] * v2[1];
     }
 }
