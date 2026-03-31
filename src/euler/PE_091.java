e0cea901c55933dfadb7516391930e07fdc580e8
laasl <LaasHendrik.Lumberg@pria.ee>
2026-03-31 12:05:53 +0300

    made 091 better

--- src/euler/PE_091.java
+++ src/euler/PE_091.java
@@ -9,10 +9,10 @@ public class PE_091 {
 
     public static String PE() {
         int n = 50;
-        return String.valueOf(rightTrianglesBelow2(n));
+        return String.valueOf(rightTrianglesBelow(n));
     }
 
-    private static long rightTrianglesBelow2(int n) {
+    private static long rightTrianglesBelow(int n) {
         long count = 3L*n*n;
         for (int x2 = 1; x2 <= n; x2++) {
             for (int x1 = 1; x1 < x2; x1++) {
@@ -21,18 +21,13 @@ public class PE_091 {
             }
         }
         for (int x1 = 1; x1 <= n; x1++) {
-            for (int x2 = 1; x2 <= x1; x2++) {
-                int xd = x2-x1;
-                for (int y1 = 1; y1 <= n; y1++) {
-                    int y2Limit = n;
-                    if (x1 == x2) y2Limit = y1;
-                    for (int y2 = 1; y2 <= y2Limit; y2++) {
-                        if (x1 == x2 && y1 == y2) continue;
-                        int yd = y2-y1;
-                        if (
-                            x1*xd + y1*yd == 0
-                        ) count += 2;
-                    }
+            for (int x2 = x1+1; x2 <= n; x2++) {
+                int xd = x1*(x2-x1);
+                for (int y1 = (int) Math.sqrt(xd)+1; y1 <= n; y1++) {
+                    if (xd % y1 != 0) continue;
+                    int y2 = y1 - xd/y1;
+                    if (y2*x1 >= (y1*x2)) continue;
+                    count += 2;
                 }
             }
         }
