45b68b71c18124f5e901b5967cb0f11d09bd7144
laasl <LaasHendrik.Lumberg@pria.ee>
2026-04-06 16:10:24 +0300

    made 121 (slightly) better

--- src/euler/PE_121.java
+++ src/euler/PE_121.java
@@ -3,17 +3,11 @@ package euler;
 import utils.Combinations;
 import utils.Fraction;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class PE_121 {
-    private static final Map<Integer, Map<Integer, Fraction<Long>>> results = new HashMap<>();
+    private static Fraction<Long>[][] results;
 
     static void main() {
-        double s = System.currentTimeMillis();
         System.out.println(PE());
-        double e = System.currentTimeMillis();
-        System.out.println((e - s) + " ms");
     }
 
     public static String PE() {
@@ -22,10 +16,9 @@ public class PE_121 {
         return String.valueOf(winChance.den / winChance.num);
     }
 
+    @SuppressWarnings("unchecked")
     private static Fraction<Long> winChance(int turns) {
-        for (int i = 1; i <= turns; i++) {
-            results.put(i, new HashMap<>());
-        }
+        results = new Fraction[turns+1][turns+1];
         int winTarget = turns / 2 + 1;
         Fraction<Long> winChance = new Fraction<>(0L);
         for (int i = winTarget; i <= turns; i++) {
@@ -38,12 +31,12 @@ public class PE_121 {
         if (winAmount > turns) return new Fraction<>(0L);
         if (winAmount == turns) return new Fraction<>(1L, Combinations.FACTORIAL[turns+1]);
         if (winAmount == 0) return new Fraction<>(1L, turns+1L);
-        if (results.get(turns).containsKey(winAmount)) return results.get(turns).get(winAmount);
+        if (results[turns][winAmount] != null) return results[turns][winAmount];
 
         Fraction<Long> sum = new Fraction<>(0L);
         sum = sum.add(chanceToGetWinAmount(winAmount-1, turns-1).multiply(new Fraction<>(1L, turns+1L)));
         sum = sum.add(chanceToGetWinAmount(winAmount, turns-1).multiply(new Fraction<>((long) turns, (long) turns+1)));
-        results.get(turns).put(winAmount, sum);
+        results[turns][winAmount] = sum;
         return sum;
     }
 }
