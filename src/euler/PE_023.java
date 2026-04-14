3162481f4e9114c2bc44f5c76e9318228c20a2c3
laasl <LaasHendrik.Lumberg@pria.ee>
2026-04-14 09:42:48 +0300

    023 remove time log

--- src/euler/PE_023.java
+++ src/euler/PE_023.java
@@ -11,10 +11,7 @@ public class PE_023 {
     private static final List<Integer> oddAbundantNumbers = new ArrayList<>();
 
     static void main() {
-        double s = System.currentTimeMillis();
         System.out.println(PE());
-        double e = System.currentTimeMillis();
-        System.out.println((e-s) + " ms");
     }
 
     public static String PE() {
