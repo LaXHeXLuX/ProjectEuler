63e944a8a31a05928023cb81bb3010d2b4f53b46
LaXHeXLuX <laas.lumberg@gmail.com>
2026-04-15 13:19:17 +0300

    made 029 better, added note

--- src/euler/PE_029.java
+++ src/euler/PE_029.java
@@ -7,16 +7,13 @@ import java.util.Arrays;
 public class PE_029 {
 
     static void main() {
-        double s = System.currentTimeMillis();
         System.out.println(PE());
-        double e = System.currentTimeMillis();
-        System.out.println((e-s) + " ms");
     }
 
     public static String PE() {
-        int limitA = 1_000_000;
-        int limitB = 1_000_000;
-        return String.valueOf(distinctPowerCount0 (limitA, limitB));
+        int limitA = 100;
+        int limitB = 100;
+        return String.valueOf(distinctPowerCount0(limitA, limitB));
     }
 
     private static long includeExclude(int p, int limitB) {
@@ -50,18 +47,12 @@ public class PE_029 {
     }
 
     private static int intersect(int min, int max, int lcm, int limitB) {
-        return min*limitB / lcm - (max*2-1) / lcm;
+        return (int) ((long) min * limitB / lcm) - (max*2-1) / lcm;
     }
 
     private static long distinctPowerCount0(int limitA, int limitB) {
-        double s = System.currentTimeMillis();
-        int P = 32 - Integer.numberOfLeadingZeros(limitA);
-        long[] counts = new long[P];
-        for (int p = 1; p < P; p++) {
-            counts[p] = includeExclude(p, limitB);
-        }
-        double e = System.currentTimeMillis();
-        System.out.println("prep: " + (e-s) + " ms");
+        long[] counts = new long[32 - Integer.numberOfLeadingZeros(limitA)];
+        Arrays.fill(counts, -1);
         int sqrt = (int) Math.sqrt(limitA);
 
         long count = 0;
@@ -69,107 +60,18 @@ public class PE_029 {
         int highSkip = 0;
         for (int a0 = 2; a0 <= sqrt; a0++) {
             if (skip[a0]) continue;
-            int max = a0*a0;
-            int p = 1;
-            while (max <= sqrt) {
-                skip[max] = true;
-                max *= a0;
-                p++;
+            long a = (long) a0 * a0;
+            int k = 1;
+            while (a <= limitA) {
+                if (a <= sqrt) skip[(int) a] = true;
+                else highSkip++;
+                a *= a0;
+                k++;
             }
-            while (max <= limitA) {
-                max *= a0;
-                p++;
-                highSkip++;
-            }
-            count += counts[p];
+            if (counts[k] == -1) counts[k] = includeExclude(k, limitB);
+            count += counts[k];
         }
-        count += (limitA - sqrt - highSkip) * (limitA - 1L);
+        count += (limitA - sqrt - highSkip) * (limitB - 1L);
         return count;
-    }
-
-    private static int maxPower(int a, int limit) {
-        long max = a;
-        int pow = 0;
-        while (max <= limit) {
-            max *= a;
-            pow++;
-        }
-        return pow;
-    }
-
-    private static long recurse(long lcm, int index, int sign, long left, long right, int[] list) {
-        if (lcm > right) return 0;
-        long res = sign * (right / lcm - (left - 1) / lcm);
-        for (int i = index + 1; i < list.length; i++) {
-            res += recurse(Diophantine.lcm(lcm, list[i]), i, -sign, left, right, list);
-        }
-        return res;
-    }
-
-    private static long dd(long left, long right, int a, int b, boolean[] composite) {
-        long res = right / b - (left - 1) / b;
-        int[] initialList = new int[b-a];
-        int index = 0;
-        for (int i = a; i < b; i++) {
-            if (!composite[i]) initialList[index++] = i;
-        }
-        int[] list = Arrays.copyOf(initialList, index);
-        for (int i = 0; i < list.length; i++) {
-            res -= recurse(Diophantine.lcm(b, list[i]), i, 1, left, right, list);
-        }
-        return res;
-    }
-
-    private static long distinctPowerCount2(int limitA, int limitB) {
-        int sqrt = (int) Math.sqrt(limitA);
-        int P = maxPower(2, limitA);
-
-        long[] counts = new long[P + 1];
-        counts[1] = limitA - 1;
-
-        for (int p = 2; p <= P; p++) {
-            // Sieve: mark composite exponents in [p, P]
-            boolean[] composite = new boolean[P + 1];
-            for (int i = p; i <= P / 2; i++) {
-                int k = i*i;
-                while (k <= P) {
-                    composite[k] = true;
-                    k += i;
-                }
-            }
-
-            long uMin = (long) (p - 1) * limitB + 1;
-            long uMax = (long) p * limitB;
-            for (int f = p; f <= P; f++) {
-                if (!composite[f]) counts[f] += dd(uMin, uMax, p, f, composite);
-            }
-        }
-
-        for (int c = 2; c <= P; c++) counts[c] += counts[c - 1];
-
-        // Harvest: iterate over bases 2..sqrt(n), skip perfect powers
-        boolean[] used = new boolean[sqrt + 1];
-        long ans = 0;
-        long coll = 0;
-        for (int i = 2; i <= sqrt; i++) {
-            if (used[i]) continue;
-
-            int c = maxPower(i, limitA);
-            ans += counts[c];
-            long u = i;
-            for (int j = 2; j <= c; j++) {
-                u *= i;
-                if (u <= sqrt) {
-                    used[(int) u] = true;
-                } else {
-                    coll += c - j + 1;
-                    break;
-                }
-            }
-        }
-
-        ans += (long) (limitA - sqrt) * (limitB - 1);
-        ans -= coll * (limitA - 1);
-        return ans;
     }
 }
