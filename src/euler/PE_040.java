1fa460ab52c053eba5de9da32461b3244e024d5c
LaXHeXLuX <laas.lumberg@gmail.com>
2026-04-21 19:50:26 +0300

    slightly changed 040, added note

--- src/euler/PE_040.java
+++ src/euler/PE_040.java
@@ -1,40 +1,38 @@
 package euler;
 
+import utils.Diophantine;
+
 public class PE_040 {
     static void main() {
         System.out.println(PE());
     }
 
     public static String PE() {
-        int[] indexes = {1, 10, 100, 1_000, 10_000, 100_000, 1_000_000};
-        return String.valueOf(productOfConstantDigits2(indexes));
+        long[] indexes = {1, 10, 100, 1_000, 10_000, 100_000, 1_000_000};
+        return String.valueOf(productOfConstantDigits(indexes));
     }
 
-    private static long productOfConstantDigits2(int[] indexes) {
+    private static long productOfConstantDigits(long[] indexes) {
         long product = 1;
-        for (int index : indexes) {
-            product *= indexAt(index-1);
+        for (long index : indexes) {
+            product *= digitAt(index-1);
         }
         return product;
     }
 
-    private static int indexAt(int index) {
-        int powTen = 1;
-        int factor = 1;
-        int x = 9*powTen*factor;
-        while (x <= index) {
-            index -= x;
-            powTen *= 10;
-            factor++;
-            x = 9*powTen*factor;
+    private static int digitAt(long i) {
+        int digits = 1;
+        long numberCount = 9;
+        long j = numberCount*digits;
+        while (j <= i) {
+            i -= j;
+            digits++;
+            numberCount *= 10;
+            j = numberCount*digits;
         }
 
-        int num = powTen + index/factor;
-        int i = factor - (index % factor) - 1;
-        for (int j = 0; j < i; j++) {
-            num /= 10;
-        }
-
-        return num % 10;
+        long num = Diophantine.pow10[digits-1] + i/digits;
+        int digitPlace = digits - 1 - (int) (i % digits);
+        return (int) ((num / Diophantine.pow10[digitPlace]) % 10);
     }
 }
