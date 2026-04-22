be0030e9f62d8499ef74378c46a152fffceb9891
LaXHeXLuX <laas.lumberg@gmail.com>
2026-04-22 21:21:46 +0300

    058 remove mod constraints, int -> long

--- src/euler/PE_058.java
+++ src/euler/PE_058.java
@@ -13,17 +13,17 @@ public class PE_058 {
         return String.valueOf(firstSpiralLengthWithPrimeDiagonalRatioBelow(fraction));
     }
 
-    private static int firstSpiralLengthWithPrimeDiagonalRatioBelow(int[] fraction) {
+    private static long firstSpiralLengthWithPrimeDiagonalRatioBelow(int[] fraction) {
         int num = fraction[0];
         int den = fraction[1];
-        int l = 3;
-        int primeCounter = 3;
+        long l = 3;
+        long primeCounter = 3;
         while (num * 2*l < primeCounter * den) {
             l += 2;
-            int ll = l*l;
-            if (l % 3 != 0 && Primes.isPrime(ll - 3*l + 3)) primeCounter++;
-            if ((l % 5) < 3 && Primes.isPrime(ll - 2*l + 2)) primeCounter++;
-            if (l % 3 != 2 && Primes.isPrime(ll - l + 1)) primeCounter++;
+            long ll = l*l;
+            if (Primes.isPrime(ll - 3*l + 3)) primeCounter++;
+            if (Primes.isPrime(ll - 2*l + 2)) primeCounter++;
+            if (Primes.isPrime(ll - l + 1)) primeCounter++;
         }
         return l;
     }
