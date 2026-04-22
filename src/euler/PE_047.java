63a95401ee0c871a9581a2e3c25beae04a5f9243
LaXHeXLuX <laas.lumberg@gmail.com>
2026-04-22 19:30:00 +0300

    047 slight refactoring

--- src/euler/PE_047.java
+++ src/euler/PE_047.java
@@ -10,23 +10,21 @@ public class PE_047 {
     public static String PE() {
         int n = 4;
         int m = 4;
-        long result = firstNConsecutiveNumbersToHaveMDistinctPrimeFactors2(n, m);
+        long result = firstNConsecutiveNumbersToHaveMDistinctPrimeFactors(n, m);
         return String.valueOf(result);
     }
 
-    private static long firstNConsecutiveNumbersToHaveMDistinctPrimeFactors2(int n, int m) {
+    private static long firstNConsecutiveNumbersToHaveMDistinctPrimeFactors(int n, int m) {
         int limit = 100;
         while (true) {
-            limit *= 5;
+            limit *= 2;
             primeFactorCounts(limit);
             int count = 0;
             for (int i = 0; i < pfCounts.length; i++) {
                 if (pfCounts[i] == m) {
-                    count++;
-                    if (count == m) return i-n+1;
-                } else {
-                    count = 0;
+                    if (++count == n) return i-n+1;
                 }
+                else count = 0;
             }
 
         }
@@ -44,7 +42,7 @@ public class PE_047 {
         for (int i = 3; i < limit; i+=2) {
             if (pfCounts[i] > 0) continue;
             int k = i;
-            while (k < limit) {
+            while (k >= 0 && k < limit) {
                 pfCounts[k]++;
                 k += i;
             }
