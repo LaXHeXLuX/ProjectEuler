229020ca357ab0451dfc5221a274836b28341465
LaXHeXLuX <laas.lumberg@gmail.com>
2026-04-23 08:36:39 +0300

    made 056 better, added note

--- src/euler/PE_056.java
+++ src/euler/PE_056.java
@@ -15,11 +15,13 @@ public class PE_056 {
     }
 
     private static int maximumDigitalSum(int aLimit, int bLimit) {
+        int maxDigits = (int) (bLimit * Math.log10(aLimit));
+        double avg = 4.5 + 1.0/Math.pow(maxDigits, 0.25);
         int maximumDigitalSum = 0;
 
         for (int a = aLimit-1; a > 0; a--) {
-            int lowerLimitB = (int) ((maximumDigitalSum / 5.0) / Math.log10(a));
-            for (int b = bLimit-1; b > lowerLimitB; b--) {
+            int bMin = (int) ((maximumDigitalSum / avg) / Math.log10(a));
+            for (int b = bLimit-1; b > bMin; b--) {
                 BigInteger power = BigInteger.valueOf(a).pow(b);
                 int digitSum = Diophantine.digitSum(power);
                 if (digitSum > maximumDigitalSum) maximumDigitalSum = digitSum;
