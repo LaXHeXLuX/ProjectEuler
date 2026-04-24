198d5b959a318b4ca46b21e0f4933f72f81b3396
LaXHeXLuX <laas.lumberg@gmail.com>
2026-04-25 00:22:21 +0300

    made 044 better, added note with results

--- src/euler/PE_044.java
+++ src/euler/PE_044.java
@@ -6,104 +6,57 @@ import utils.Primes;
 
 public class PE_044 {
     static void main() {
-        double s = System.currentTimeMillis();
         System.out.println(PE());
-        double e = System.currentTimeMillis();
-        System.out.println((e-s) + " ms");
     }
 
     public static String PE() {
         int count = 1;
-        double s = System.currentTimeMillis();
-        System.out.println(pentagonalDifferences(count));
-        double e = System.currentTimeMillis();
-        System.out.println("1: " + (e-s) + " ms");
-        return String.valueOf(smallestDifferenceWithProperty());
+        long result = pentagonalDifferences(count);
+        return String.valueOf(result);
     }
 
     private static long pentagonalDifferences(int count) {
         int found = 0;
-
-        long y = -1;
-        while (found != count) {
-            y += 6;
-            long product = (y-1)*(y+1);
-            Primes.PF[] pfs = Primes.primeFactorsProduct(y+1, 1, y-1);
-            if (pfs[0].power <= pfs[1].power) continue;
-            long pow6 = Diophantine.pow(6, pfs[1].power);
-            //long exp2 = pfs[0].power - pfs[1].power;
-            pfs[0].power -= pfs[1].power;
+        long i = 0;
+        while (i >= 0) {
+            i++;
+            long PD = pentagonal(i);
+            long a = 6* i - 1;
+            Primes.PF[] pfs = Primes.primeFactorsProduct(a/2 + 1, 1, a/2);
+            if (pfs[0].primeFactor != 2) continue;
+            long pow3 = Diophantine.pow(3, pfs[1].power);
             pfs[1].power = 0;
-
+            long product = (a/2)*(a/2 + 1) / pow3;
             long[] divisors = Divisors.divisors(pfs);
-            int kLimit = (int) (Math.sqrt(product) / pow6/* / (1L << exp2)*/);
-            for (long k : divisors) {
-                if (k > kLimit) break;
-//                long d1;
-//                long d2;
-//                if (pow6 == 6) {
-//                    for (int i = 0; i < exp2-2; i++) {
-//                        d1 = pow6 * (1L << i);
-//                        d2 = product / d1;
-//                        if (pentagonalDifference(y, d1, d2)) found++;
-//                    }
-//                }
-//                else {
-//                    d1 = pow6 * (1L << (exp2-1));
-//                    d2 = product / d1;
-//                    if (pentagonalDifference(y, d1, d2)) found++;
-//                }
-                long d1 = pow6 * k;
+            long d1Limit = (long) Math.sqrt((double) product / pow3);
+            for (long d1 : divisors) {
+                if (d1 > d1Limit) break;
                 long d2 = product / d1;
-                if (pentagonalDifference(y, d1, d2)) found++;
+                d1 *= pow3;
+                long c = d1+d2;
+                if (c % 6 != 5) continue;
+                long b = d2-d1;
+                if (b % 6 != 5) continue;
+
+                long j = (1 + b) / 6;
+                long k = (1 + c) / 6;
+                long P1 = pentagonal(j);
+                long P2 = pentagonal(k);
+                long P3 = P1 + P2;
+                if (isPentagonal(P3)) {
+                    if (++found == count) return PD;
+                }
             }
         }
-
         return -1;
-    }
-
-    private static boolean pentagonalDifference(long y, long d1, long d2) {
-        if (d2 % 2 != 0) return false; //throw new RuntimeException("BAD");
-        if (d2 % 6 != 4) return false;
-        long sum = d1+d2;
-        if (sum % 12 != 10) return false; //throw new RuntimeException("sum = d1 + d2: " + d1 + " + " + d2 + " = " + sum + " % 12 = " + (sum % 12) + ". y p: " + y + " " + product);
-        long x = (d1 + d2)/2;
-        long m = d2 - x;
-        if (x % 6 != 5) throw new RuntimeException("x: " + x + " % 6 = " + (x%6) + ". y: " + y + ", d1 d2: " + d1 + " " + d2);
-        if (m % 6 != 5) throw new RuntimeException("m: " + m + " % 6 = " + (m%6) + ". y: " + y + ", d1 d2: " + d1 + " " + d2);
-        long nD = (1+y)/6;
-        long n1 = (1+m)/6;
-        long n2 = (1+x)/6;
-        long P3 = pentagonal(n1) + pentagonal(n2);
-        if (isPentagonal(P3)) {
-            System.out.println("found " + nD + " " + n1 + " " + n2 + ": " + pentagonal(nD) + " " + pentagonal(n1) + " " + pentagonal(n2));
-            return true;
-        }
-        return false;
     }
 
     private static long pentagonal(long n) {
         return n*(3*n - 1) / 2;
     }
 
-    private static boolean isPentagonal(long x) {
-        long n = (long) ((Math.sqrt(24 * x + 1) + 1) / 6);
-        return pentagonal(n) == x;
-    }
-
-    private static long smallestDifferenceWithProperty() {
-        long smallestDiff = Long.MAX_VALUE;
-        for (int n2 = 2; ; n2++) {
-            long p2 = pentagonal(n2);
-            for (int n1 = n2-1; n1 > 0; n1--) {
-                long p1 = pentagonal(n1);
-                if (p1 >= smallestDiff && p2 >= smallestDiff) break;
-                if (isPentagonal(p1 + p2)) {
-                    if (isPentagonal(p1 + 2 * p2) && p1 < smallestDiff) smallestDiff = p1;
-                    if (isPentagonal(2 * p1 + p2) && p2 < smallestDiff) smallestDiff = p2;
-                }
-            }
-            if (3L*n2 - 2 > smallestDiff) return smallestDiff;
-        }
+    private static boolean isPentagonal(long P) {
+        long n = (long) ((Math.sqrt(24 * P + 1) + 1) / 6);
+        return pentagonal(n) == P;
     }
 }
