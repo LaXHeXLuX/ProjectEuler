9360e63929afe22c62cb344de3d68f4b4dcbf2ad
laasl <LaasHendrik.Lumberg@pria.ee>
2026-05-13 16:57:53 +0300

    204 removed memoization, added note results

--- src/euler/PE_204.java
+++ src/euler/PE_204.java
@@ -4,7 +4,6 @@ import utils.Primes;
 
 public class PE_204 {
     private static int[] primes;
-    private static long[][] memo;
 
     static void main() {
         System.out.println(PE());
@@ -18,15 +17,12 @@ public class PE_204 {
 
     private static long hammingNumbers(int type, long limit) {
         primes = Primes.primes(type+1);
-        int memoSize = (int) Math.pow(limit, 0.25);
-        if (memoSize < primes[primes.length-1]) memoSize = primes[primes.length-1];
-        memo = new long[primes.length][memoSize];
         return hamming(primes.length-1, limit) + 1;
     }
 
     private static long hamming(int index, long limit) {
         if (index == 0) return 63 - Long.numberOfLeadingZeros(limit);
-        if (limit < memo[index].length) return hamming(index, (int) limit);
+        if (limit <= Integer.MAX_VALUE) return hamming(index, (int) limit);
 
         return hamming(index-1, limit) + hamming(index, limit/primes[index]) + 1;
     }
@@ -35,9 +31,6 @@ public class PE_204 {
         if (i == 0) return 31 - Integer.numberOfLeadingZeros(L);
         if (L < primes[i]) return hamming(i-1, L);
 
-        if (memo[i][L] != 0) return memo[i][L];
-        long result = hamming(i-1, L) + hamming(i, L/primes[i]) + 1;
-        memo[i][L] = result;
-        return result;
+        return hamming(i-1, L) + hamming(i, L/primes[i]) + 1;
     }
 }
