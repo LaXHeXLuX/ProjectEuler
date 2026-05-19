d6950b2b6047745de38feb42df84e47de8060a1c
laasl <LaasHendrik.Lumberg@pria.ee>
2026-05-19 10:56:57 +0300

    made 064 cleaner, added note

--- src/euler/PE_064.java
+++ src/euler/PE_064.java
@@ -1,7 +1,5 @@
 package euler;
 
-import utils.Diophantine;
-
 public class PE_064 {
     static void main() {
         System.out.println(PE());
@@ -16,11 +14,28 @@ public class PE_064 {
         int counter = 0;
 
         for (int i = 2; i <= limit; i++) {
-            int len = Diophantine.continuedFractionLength(i);
+            int len = continuedFractionLength(i);
             if (len % 2 == 1) counter++;
         }
 
         return counter;
+    }
+
+    private static int continuedFractionLength(int n) {
+        int a0 = (int) Math.sqrt(n);
+        if (a0*a0 == n) return 0;
+        int a;
+        int m = a0;
+        int d = 1;
+        int len = 0;
+        do {
+            len++;
+            d = (n - m*m)/d;
+            a = (a0 + m)/d;
+            m = a*d - m;
+        } while (!(m == a0 && d == 1));
+
+        return len;
     }
 }
 
--- src/utils/Diophantine.java
+++ src/utils/Diophantine.java
@@ -5,23 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Diophantine {
-    public static int continuedFractionLength(int n) {
-        int a0 = (int) Math.sqrt(n);
-        if (a0*a0 == n) return 0;
-        int d1 = n - a0*a0;int a1 = 2*a0/d1;
-        int m = a1*d1 - a0;
-        int d = (n - m*m)/d1;
-        int a = (a0 + m)/d;
-        int len = 1;
-        while (!(m == a0 && d == d1)) {
-            len++;
-            m = d*a - m;
-            d = (n - m*m)/d;
-            a = (a0 + m)/d;
-        }
-
-        return len;
-    }
     public static int[] continuedFraction(int n) {
         int a0 = (int) Math.sqrt(n);
         if (a0*a0 == n) return new int[] {a0};
