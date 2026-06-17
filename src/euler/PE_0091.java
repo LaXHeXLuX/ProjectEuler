0054283066cccb562b602073dc21bb6966b11594
LaXHeXLuX <laas.lumberg@gmail.com>
2026-06-17 17:40:35 +0300

    made 0091 better

--- src/euler/PE_0091.java
+++ src/euler/PE_0091.java
@@ -8,29 +8,33 @@ public class PE_0091 {
     }
 
     public static String PE() {
-        int n = 50;
-        return String.valueOf(rightTrianglesBelow(n));
+        int L = 50;
+        return String.valueOf(rightTriangles(L));
     }
 
-    private static long rightTrianglesBelow(int n) {
-        long count = 3L*n*n;
-        for (int x2 = 1; x2 <= n; x2++) {
-            for (int x1 = 1; x1 < x2; x1++) {
-                int y2 = Diophantine.root(x1*(x2-x1));
-                if (y2 > 0) count += 2;
+    private static long rightTriangles(int L) {
+        long count = 3L*L*L;
+
+        for (int y1 = 1; y1 <= L; y1++) {
+            for (int y2 = 1; y2 < y1; y2++) {
+                int x2 = Diophantine.root(y2*(y1-y2));
+                if (x2 > 0) count += 2;
             }
         }
-        for (int x1 = 1; x1 <= n; x1++) {
-            for (int x2 = x1+1; x2 <= n; x2++) {
-                int xd = x1*(x2-x1);
-                for (int y1 = (int) Math.sqrt(xd)+1; y1 <= n; y1++) {
+
+        for (int x1 = 1; x1 <= L; x1++) {
+            for (int x2 = x1+1; x2 <= L; x2++) {
+                int xd = x1*x1 - x1*x2;
+                for (int y1 = 1; y1 <= L; y1++) {
                     if (xd % y1 != 0) continue;
-                    int y2 = y1 - xd/y1;
-                    if (y2*x1 >= (y1*x2)) continue;
+                    int y2 = xd / y1 + y1;
+                    if (y2 <= 0 || y2 > L) continue;
+                    if (y1 == y2 && x1 == x2) continue;
                     count += 2;
                 }
             }
         }
+
         return count;
     }
 }
