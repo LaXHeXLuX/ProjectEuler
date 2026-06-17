c5cfb72530c6d4f55087c05430b229bea0190944
LaXHeXLuX <laas.lumberg@gmail.com>
2026-06-17 18:42:11 +0300

    made 0091 better

--- src/euler/PE_0091.java
+++ src/euler/PE_0091.java
@@ -17,19 +17,19 @@ public class PE_0091 {
 
         for (int y1 = 1; y1 <= L; y1++) {
             for (int y2 = 1; y2 < y1; y2++) {
-                int x2 = Diophantine.root(y2*(y1-y2));
-                if (x2 > 0) count += 2;
+                if (Diophantine.root((long) y2 * (y1-y2)) > 0) count += 2;
             }
         }
 
         for (int x1 = 1; x1 <= L; x1++) {
-            for (int x2 = x1+1; x2 <= L; x2++) {
-                int xd = x1*x1 - x1*x2;
-                for (int y1 = 1; y1 <= L; y1++) {
+            for (int x2 = 1; x2 < x1; x2++) {
+                long xd = (long) x1 * (x1 - x2);
+                double sqrt = Math.sqrt((long) L * L - 4L*x1*(x1-x2));
+                int low = (int) Math.ceil((L - sqrt)/2);
+                if (low < 1) low = 1;
+                int high = (int) Math.floor((L + sqrt)/2);
+                for (int y1 = low; y1 <= high; y1++) {
                     if (xd % y1 != 0) continue;
-                    int y2 = xd / y1 + y1;
-                    if (y2 <= 0 || y2 > L) continue;
-                    if (y1 == y2 && x1 == x2) continue;
                     count += 2;
                 }
             }
