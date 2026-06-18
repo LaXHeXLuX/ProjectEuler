eae8fceb27846b5b73238fbac3c86abc621933b6
LaXHeXLuX <laas.lumberg@gmail.com>
2026-06-18 20:21:42 +0300

    0092 remove time log

--- src/euler/PE_0092.java
+++ src/euler/PE_0092.java
@@ -6,10 +6,7 @@ public class PE_0092 {
     private static long[][] cache;
 
     static void main() {
-        double s = System.currentTimeMillis();
         System.out.println(PE());
-        double e = System.currentTimeMillis();
-        System.out.println((e-s) + " ms");
     }
 
     public static String PE() {
