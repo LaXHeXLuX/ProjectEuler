0389567e79ee066c1d2ef5e544a4318807289a86
LaXHeXLuX <laas.lumberg@gmail.com>
2026-06-19 23:04:07 +0300

    made 0104 (slightly) better, added note

--- src/euler/PE_0104.java
+++ src/euler/PE_0104.java
@@ -1,8 +1,11 @@
 package euler;
 
+import utils.Pandigital;
+
 public class PE_0104 {
     private static final double LOG_GOLDEN = Math.log10((1 + Math.sqrt(5))/2);
     private static final double LOG_SQRT5 = Math.log10(5) / 2;
+
     static void main() {
         System.out.println(PE());
     }
@@ -20,8 +23,8 @@ public class PE_0104 {
             f2 = (f2 + f1) % 1_000_000_000;
             f1 = temp;
             counter++;
-            if (isPandigital(f2)) {
-                if (isPandigital(first9DigitsOfFibonacciN(counter))) return counter;
+            if (Pandigital.isPandigital(f2)) {
+                if (Pandigital.isPandigital(first9DigitsOfFibonacciN(counter))) return counter;
             }
         }
     }
@@ -30,17 +33,5 @@ public class PE_0104 {
         double logFn = n * LOG_GOLDEN - LOG_SQRT5;
         double exp = logFn - Math.floor(logFn) + 8;
         return (int) Math.pow(10, exp);
-    }
-
-    private static boolean isPandigital(int n) {
-        if (n < 123456789 || n > 987654321) return false;
-        boolean[] digits = new boolean[9];
-        while (n > 0) {
-            int digit = n % 10;
-            if (digit == 0 || digits[digit-1]) return false;
-            digits[digit-1] = true;
-            n /= 10;
-        }
-        return true;
     }
 }
