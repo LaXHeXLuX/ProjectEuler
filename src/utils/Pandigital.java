6f9e7372c479e2d136c54e4ef025c6b2e895de9e
LaXHeXLuX <laas.lumberg@gmail.com>
2026-06-19 23:05:57 +0300

    Pandigital - remove base method because unused

--- src/utils/Pandigital.java
+++ src/utils/Pandigital.java
@@ -12,21 +12,4 @@ public class Pandigital {
         }
         return true;
     }
-    public static boolean isPandigital(long n, int base) {
-        int[] digits = new int[base];
-        while (n > 0) {
-            digits[(int) (n % base)]++;
-            n /= base;
-        }
-
-        if (digits[0] > 0) return false;
-        int i;
-        for (i = 1; i < digits.length; i++) {
-            if (digits[i] != 1) break;
-        }
-        for (; i < digits.length; i++) {
-            if (digits[i] != 0) return false;
-        }
-        return true;
-    }
 }
