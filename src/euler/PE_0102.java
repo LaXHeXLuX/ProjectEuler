e9a7b5f2586a8c011a5000f91c26cba1d2ab7914
LaXHeXLuX <laas.lumberg@gmail.com>
2026-06-19 19:39:33 +0300

    made 0102 better, added note

--- src/euler/PE_0102.java
+++ src/euler/PE_0102.java
@@ -2,17 +2,7 @@ package euler;
 
 import utils.Parser;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class PE_0102 {
-    private record Triangle(Point a, Point b, Point c) {
-        private record Point(int x, int y) {
-            private int cross(Point p) {
-                return x * p.y - p.x * y;
-            }
-        }
-    }
 
     static void main() {
         System.out.println(PE());
@@ -20,9 +10,9 @@ public class PE_0102 {
 
     public static String PE() {
         String file = "src/euler/inputs/PE_0102_triangles.txt";
-        List<Triangle> triangles = parse(file);
+        int[][] triangles = Parser.parseManyInts(file, ",");
         int count = 0;
-        for (Triangle triangle : triangles) {
+        for (int[] triangle : triangles) {
             if (containsOrigin(triangle)) {
                 count++;
             }
@@ -30,26 +20,14 @@ public class PE_0102 {
         return String.valueOf(count);
     }
 
-    private static boolean containsOrigin(Triangle t) {
-        boolean crossAB = t.a.cross(t.b) > 0;
-        boolean crossBC = t.b.cross(t.c) > 0;
-        boolean crossCA = t.c.cross(t.a) > 0;
+    private static boolean containsOrigin(int[] t) {
+        boolean crossAB = cross(t[0], t[1], t[2], t[3]) > 0;
+        boolean crossBC = cross(t[2], t[3], t[4], t[5]) > 0;
+        boolean crossCA = cross(t[4], t[5], t[0], t[1]) > 0;
         return crossAB == crossBC && crossBC == crossCA;
     }
 
-    private static List<Triangle> parse(String file) {
-        int[][] ints = Parser.parseManyInts(file, ",");
-        List<Triangle> triangles = new ArrayList<>();
-
-        for (int[] row : ints) {
-            Triangle t = new Triangle(
-                    new Triangle.Point(row[0], row[1]),
-                    new Triangle.Point(row[2], row[3]),
-                    new Triangle.Point(row[4], row[5])
-            );
-            triangles.add(t);
-        }
-
-        return triangles;
+    private static int cross(int x1, int y1, int x2, int y2) {
+        return x1 * y2 - x2 * y1;
     }
 }
