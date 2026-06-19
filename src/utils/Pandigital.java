f7fc3d708e06b82e73778e2c19a357c9a82a17ff
LaXHeXLuX <laas.lumberg@gmail.com>
2026-06-19 23:04:34 +0300

    Pandigital - added separate method for 10

--- src/utils/Pandigital.java
+++ src/utils/Pandigital.java
@@ -2,7 +2,15 @@ package utils;
 
 public class Pandigital {
     public static boolean isPandigital(long n) {
-        return isPandigital(n, 10);
+        if (n < 123456789 || n > 987654321) return false;
+        boolean[] digits = new boolean[9];
+        while (n > 0) {
+            int digit = (int) n % 10;
+            if (digit == 0 || digits[digit-1]) return false;
+            digits[digit-1] = true;
+            n /= 10;
+        }
+        return true;
     }
     public static boolean isPandigital(long n, int base) {
         int[] digits = new int[base];
