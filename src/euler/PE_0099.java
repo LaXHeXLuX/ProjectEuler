8d6e4f3c0e2e7f7ff39a7cd8dbb448a5e4942b37
LaXHeXLuX <laas.lumberg@gmail.com>
2026-06-19 18:27:40 +0300

    made 0099 better, added (tiny) note

--- src/euler/PE_0099.java
+++ src/euler/PE_0099.java
@@ -2,39 +2,31 @@ package euler;
 
 import utils.Parser;
 
-import java.util.*;
-
 public class PE_0099 {
-    private record BaseExp(int base, int exp, int index) {}
-
     static void main() {
         System.out.println(PE());
     }
 
     public static String PE() {
         String file = "src/euler/inputs/PE_0099_exp.txt";
-        BaseExp[] baseExponents = parse(file);
-        BaseExp biggestBaseExp = biggestBaseExp(baseExponents);
-        return String.valueOf(biggestBaseExp.index + 1);
+        double[] logValues = parse(file);
+        return String.valueOf(biggestIndex(logValues) + 1);
     }
 
-    private static BaseExp biggestBaseExp(BaseExp[] baseExponents) {
-        BaseExp biggest = baseExponents[0];
-        for (BaseExp baseExponent : baseExponents) {
-            double x = Math.log(biggest.base) / Math.log(baseExponent.base);
-            if (x * biggest.exp < baseExponent.exp) {
-                biggest = baseExponent;
-            }
+    private static int biggestIndex(double[] logValues) {
+        int biggest = 0;
+        for (int i = 1; i < logValues.length; i++) {
+            if (logValues[i] > logValues[biggest]) biggest = i;
         }
         return biggest;
     }
 
-    private static BaseExp[] parse(String filename) {
+    private static double[] parse(String filename) {
         int[][] lines = Parser.parseManyInts(filename, ",");
-        BaseExp[] baseExponents = new BaseExp[lines.length];
+        double[] logValues = new double[lines.length];
         for (int i = 0; i < lines.length; i++) {
-            baseExponents[i] = new BaseExp(lines[i][0], lines[i][1], i);
+            logValues[i] = lines[i][1] * Math.log(lines[i][0]);
         }
-        return baseExponents;
+        return logValues;
     }
 }
