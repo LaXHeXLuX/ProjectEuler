0108eaf8be80a8aad0c47431172ecb30c7b5258e
laasl <LaasHendrik.Lumberg@pria.ee>
2026-06-25 13:23:45 +0300

    made 0387 better

--- src/euler/PE_0387.java
+++ src/euler/PE_0387.java
@@ -1,11 +1,6 @@
 package euler;
 
-import utils.Diophantine;
 import utils.Primes;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class PE_0387 {
     static void main() {
@@ -13,54 +8,34 @@ public class PE_0387 {
     }
 
     public static String PE() {
-        int powTen = 14;
-        long result = sumOfStrongRightTruncatableHarshadPrimes(powTen);
-        return String.valueOf(result);
+        int powTen = 17;
+        return String.valueOf(sumOfHarshadPrimes(powTen));
     }
 
-    private static long sumOfStrongRightTruncatableHarshadPrimes(int powTenLimit) {
+    private static long sumOfHarshadPrimes(int digits) {
         long sum = 0;
-
-        List<Long> working1 = Arrays.asList(1L, 3L, 5L, 7L, 9L);
-        List<Long> working2 = Arrays.asList(2L, 4L, 6L, 8L);
-        for (int pow = 2; pow < powTenLimit; pow++) {
-            working1 = newWorking(working1, null);
-            sum += working1.removeLast();
-            working2 = newWorking(working2, working1);
-            sum += working2.removeLast();
+        for (int i = 1; i < 10; i++) {
+            sum += sumOfHarshadPrimes(i, i, digits-1);
         }
-
         return sum;
     }
 
-    private static List<Long> newWorking(List<Long> working, List<Long> workingOdd) {
-        List<Long> newWorking = new ArrayList<>();
-        long sum = 0;
-        for (Long l : working) {
-            int digitSum = Diophantine.digitSum(l);
-            newWorking.add(10*l);
-            int step = 1;
-            if (workingOdd == null) step = 2;
-            for (int i = step; i < 10; i+=step) {
-                long n = 10*l + i;
-                if (n % (digitSum+i) != 0) continue;
-                if (workingOdd == null || i % 2 == 0) newWorking.add(n);
-                else workingOdd.add(n);
-                if (Primes.isPrime(n / (digitSum+i))) {
-                    sum += sumFor(n);
-                }
+    private static long sumOfHarshadPrimes(long n, int sum, int digits) {
+        if (n % sum != 0) return 0;
+
+        long result = 0;
+        if (Primes.isPrime(n / sum)) {
+            for (int i = 1; i < 10; i += 2) {
+                long p = 10*n + i;
+                if (Primes.isPrime(p)) result += p;
             }
         }
-        newWorking.add(sum);
-        return newWorking;
-    }
+        if (digits == 1) return result;
 
-    private static long sumFor(long n) {
-        long sum = 0;
-        for (int i = 1; i < 10; i+=2) {
-            long N = 10*n + i;
-            if (Primes.isPrime(N)) sum += N;
+        for (int i = 0; i < 10; i++) {
+            result += sumOfHarshadPrimes(10*n + i, sum + i, digits-1);
         }
-        return sum;
+
+        return result;
     }
 }
