1391545737636cbc4f8421b2f8b91c5ad3f03a52
laasl <LaasHendrik.Lumberg@pria.ee>
2026-07-17 13:31:17 +0300

    0358 remove time log

--- src/euler/PE_0358.java
+++ src/euler/PE_0358.java
@@ -6,10 +6,7 @@ import utils.Primes;
 
 public class PE_0358 {
     static void main() {
-        double s = System.currentTimeMillis();
         System.out.println(PE());
-        double e = System.currentTimeMillis();
-        System.out.println((e-s) + " ms");
     }
 
     public static String PE() {
